2f1a58b23a742bc6c17e0f57c0b36bb1aaab1235
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-05-17 23:43:02 +0530

    Initial commit

---
+++ prob3.py
@@ -0,0 +1,71 @@
+
+def get_point_from_int(integer_value):
+    row = integer_value / 8
+    column = integer_value - (8 * row)
+
+    return row,column
+
+# Any value 0-63 is proper value -1 means the value is invalid
+def int_value_from_point(row, column):
+    # Boundary condition
+    if row < 0 or column < 0 or row > 7 or column > 7:
+        return -1
+
+    return 8*row + column
+
+def get_all_valid_moves(source):
+    val = -1
+    valid_moves = []
+
+    x,y = get_point_from_int(source);
+
+    val = int_value_from_point(x-2, y-1)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x-2, y+1)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x+2, y-1)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x+2, y+1)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x+1, y-2)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x-1, y-2)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x+1, y+2)
+    if val != -1 :
+        valid_moves.append(val)
+
+    val = int_value_from_point(x-1, y+2)
+    if val != -1 :
+        valid_moves.append(val)
+
+    # Returning all the valid moves
+    return valid_moves
+
+# Main method to perform the task
+def find_min_distance(source, destination, turn_count):
+    # Handle the base case
+    if source == destination :
+        return turn_count
+
+    # Get all possible valid moves
+    moves = get_all_valid_moves(source)
+    print(moves)
+
+    return turn_count
+
+print( find_min_distance(19, 36, 0 ) )              # Output should be 1
+print( find_min_distance(0, 1, 0) )                # Output should be 3
+#print( find_min_distance(17, 1, 0) )                
