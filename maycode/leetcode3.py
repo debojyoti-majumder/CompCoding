56fed29fdfc0d85f60409c1f6208167a973fec7c
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-05-29 18:05:03 +0530

    Pretty good solution
    Still 2 more test cases are getting time limit error

--- maycode/leetcode3.py
+++ maycode/leetcode3.py
@@ -1,45 +1,66 @@
 # Problem url: https://leetcode.com/problems/count-primes/#/description
 
 class Solution(object):
+    def get_nextNumber(self, number_list, start_number):
+        # Should return the value
+        for i in range(start_number, len(number_list)):
+            if not number_list[i]:
+                return i
+        
+        # Return -1 to say no more items found
+        return -1
+
+    def get_false_count(self, number_list):
+        counter = 0
+        
+        for item in number_list:
+            if not item:
+                counter = counter + 1
+
+        return counter
+    
     def countPrimes(self, n):
-        number_list = []
+        visited_number_list = []
         
         if n == 0 or n == 1 or n==2:
             return 0
         
         # Creating the number list
-        for i in range(2, n):
-            number_list.append(i)
+        for i in range(n):
+            visited_number_list.append(False)
+
+        visited_number_list[0] = True
+        visited_number_list[1] = True
 
         # Intilizing variables
         list_index = 0
         no_new_number_found = False
+        start_number = 2
 
         while not no_new_number_found:
-            item = number_list[list_index]
+            item = self.get_nextNumber(visited_number_list,start_number)
             multuiplier = 2
             no_new_number_found = True
 
+            # This just avoid the inner loop
+            if item == -1:
+                item = n
+
             val = item * multuiplier
         
-            while val <= n:
-                try:
-                    item_index = number_list.index(val)
-                    number_list.pop(item_index)
+            while val < n:
+                if not visited_number_list[val]:
+                    visited_number_list[val] = True
                     no_new_number_found = False
-
-                except ValueError:
-                    pass
 
                 multuiplier = multuiplier + 1
                 val = item * multuiplier
+            
+            start_number = item + 1
 
-            # go to the next item in the list
-            list_index = list_index + 1
-
-        # 2 is not treated as prime
-        return len(number_list)
+        return self.get_false_count(visited_number_list)
 
 s = Solution()
 print(s.countPrimes(10))
-print(s.countPrimes(499979))        # Got a time limit problem
+print(s.countPrimes(499979))        # This time limit went away
+print(s.countPrimes(999983))        # This one is getting stuck
