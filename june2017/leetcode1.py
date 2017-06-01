fe31cf061ce097531e6e232016658ee1e9863316
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-06-01 18:38:25 +0530

    Fixed the issue, problem submitted
    I should have broken after finding the all the Lates

--- june2017/leetcode1.py
+++ june2017/leetcode1.py
@@ -9,6 +9,7 @@ class Solution(object):
 
     def __check_for_late(self, s):
         lates_count = 0
+        is_late = False
 
         for i in range(len(s)):
             if lates_count > 0 :
@@ -19,24 +20,25 @@ class Solution(object):
             else:
                 if s[i] == 'L':
                     lates_count = 1
-        
-        if lates_count > 2:
-            return True
-        else:
-            return False
+            
+            if lates_count > 2:
+                is_late = True
+                break
+
+        return is_late
 
     def checkRecord(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        bad_student = False
-        bad_student = self.__check_for_late(s) or self.__check_for_absent(s)
-
-        return not bad_student
+        is_late = self.__check_for_late(s)
+        is_absent = self.__check_for_absent(s)
+        
+        return not (is_late or is_absent)
 
 s = Solution()
 
 print(s.checkRecord('PPALLP'))      # Should return true
 print(s.checkRecord('PPALLL'))      # Should return false
-print(s.checkRecord('LLLALL'))      # This test case failing
+print(s.checkRecord('LLLALL'))      # Now this passes
