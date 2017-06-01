1ec49ebf4b13ad04ed15f26f05ac3066a182496d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-06-01 18:05:50 +0530

    Some test cases are still failing
    Have look in the description

---
+++ june2017/leetcode1.py
@@ -0,0 +1,42 @@
+# Problem url: https://leetcode.com/problems/student-attendance-record-i/#/description
+
+class Solution(object):
+    def __check_for_absent(self, s): 
+        if s.count('A') > 1:
+            return True
+        else:
+            return False
+
+    def __check_for_late(self, s):
+        lates_count = 0
+
+        for i in range(len(s)):
+            if lates_count > 0 :
+                if s[i] == 'L':
+                    lates_count = lates_count + 1
+                else:
+                    lates_count = 0
+            else:
+                if s[i] == 'L':
+                    lates_count = 1
+        
+        if lates_count > 2:
+            return True
+        else:
+            return False
+
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bad_student = False
+        bad_student = self.__check_for_late(s) or self.__check_for_absent(s)
+
+        return not bad_student
+
+s = Solution()
+
+print(s.checkRecord('PPALLP'))      # Should return true
+print(s.checkRecord('PPALLL'))      # Should return false
+print(s.checkRecord('LLLALL'))      # This test case failing
