4bf294ebc5f013a6373b9b78f560251e23bddf70
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-06-06 17:38:37 +0530

    Skeleton code with test cases

---
+++ june2017/leetcode2.py
@@ -0,0 +1,13 @@
+# Problem description: https://leetcode.com/problems/first-unique-character-in-a-string/#/description
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+    
+s = Solution()
+
+print(s.firstUniqChar('leetcode'))      # Should print 0 
+print(s.firstUniqChar('loveleetcode'))  # Should print 2
