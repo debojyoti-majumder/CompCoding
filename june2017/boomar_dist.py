431e82c940e046ca6fb66f8d64b5f2afbe810ef1
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-06-07 23:36:49 +0530

    Logic corrected, but TLE error

--- june2017/boomar_dist.py
+++ june2017/boomar_dist.py
@@ -1,4 +1,5 @@
 # Problem URL:https://leetcode.com/problems/number-of-boomerangs/#/description
+import itertools
 
 class Solution(object):
     def numberOfBoomerangs(self, points):
@@ -7,30 +8,15 @@ class Solution(object):
         :rtype: int
         """
         boomerang_counter = 0
-
-        permute_iterator = self.all_perms(points)
         
-        while True:
-            try:
-                target_item = permute_iterator.next()
-                
-                # Increment the counter if it is a boomereng
-                if self.is_boomerang(target_item):
+        combinations = list(itertools.combinations(points,3))
+        for comb in combinations:
+            permuted_items = list(itertools.permutations(comb))
+            for item in permuted_items:
+                if self.is_boomerang(item):
                     boomerang_counter += 1
-            except StopIteration:
-                break
             
         return boomerang_counter
-
-    # Generates all the permutation
-    def all_perms(self,elements):
-        if len(elements) <=1:
-            yield elements
-        else:
-            for perm in self.all_perms(elements[1:]):
-                for i in range(len(elements)):
-                    # nb elements[0:1] works in both string and list contexts
-                    yield perm[:i] + elements[0:1] + perm[i:]
 
     def get_distance(self, point1, point2):
         sq_val = (point1[0] - point2[0]) ** 2 
@@ -53,5 +39,5 @@ s = Solution()
 
 #Explanation:
 #The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
-print(s.numberOfBoomerangs([1,2,3]))                # Should output 2
-print(s.numberOfBoomerangs([1,2,3,4,5]))            # Should output 20
+print(s.numberOfBoomerangs([[0,0],[1,0],[2,0]]))                # Should output 2
+print(s.numberOfBoomerangs([[0,0],[1,0],[-1,0],[0,1],[0,-1]]))  # Should output 20
