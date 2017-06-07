4aab4c773cdb13aaf2267dc8d432b409f559c339
Debojyoti Majumder <debojyoti.majumder@harman.com>
2017-06-07 12:07:09 +0530

    Adding starter code

---
+++ june2017/array_intersect2.py
@@ -0,0 +1,16 @@
+#Problem URL: https://leetcode.com/problems/intersection-of-two-arrays-ii/#/description
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        return_list = []
+
+        return return_list
+
+s = Solution()
+print(s.intersect([1,2,2,1],[2,2]))    # Should output [2,2]
