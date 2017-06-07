3a34cd33afb34c05553c3c244d994d9eed11593c
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-06-07 22:34:22 +0530

    Added permutation capability

--- june2017/boomar_dist.py
+++ june2017/boomar_dist.py
@@ -8,8 +8,26 @@ class Solution(object):
         """
         boomerang_counter = 0
 
-        return boomerang_counter
+        permute_iterator = self.all_perms(points)
         
+        while True:
+            try:
+                target_item = permute_iterator.next()
+                print(target_item)
+            except StopIteration:
+                break
+            
+        return boomerang_counter
+
+    def all_perms(self,elements):
+        if len(elements) <=1:
+            yield elements
+        else:
+            for perm in self.all_perms(elements[1:]):
+                for i in range(len(elements)):
+                    # nb elements[0:1] works in both string and list contexts
+                    yield perm[:i] + elements[0:1] + perm[i:]
+
 s = Solution()
 
 #Explanation:
