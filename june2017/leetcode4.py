a0501abb86265c0cb7402c590c08054add6a615f
Debojyoti Majumder <debojyoti.majumder@harman.com>
2017-06-07 12:02:24 +0530

    Solution accecpted, run in nlogn

--- june2017/leetcode4.py
+++ june2017/leetcode4.py
@@ -1,6 +1,30 @@
-# Dummy file from windows
+# Problem URL: https://leetcode.com/problems/intersection-of-two-arrays/#/description
 
 class Solution(object):
-    
-    def some_method(self):
-        return 0
+    def unique_numbers(self, numbers):
+        un_numbers = []
+
+        for number in numbers:
+            if number not in un_numbers:
+                un_numbers.append(number)
+        
+        return un_numbers
+
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        numbers = self.unique_numbers(nums1)
+        return_list = []
+
+        for num in numbers:
+            if num in nums2:
+                return_list.append(num)
+        
+        return return_list
+
+s = Solution()
+print(s.intersection([1, 2, 2, 1], [2, 2]))    # Should return [2]
