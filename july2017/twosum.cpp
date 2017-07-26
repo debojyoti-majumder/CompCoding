bbeba5b34782298071831a04bdf27b4912b71d0e
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-07-26 12:19:10 +0530

    Adding some test cases

--- july2017/twosum.cpp
+++ july2017/twosum.cpp
@@ -10,6 +10,9 @@ class Solution {
 	public:
 		vector<int> twoSum(vector<int>& numbers, int target) {
 			vector<int> indexes(2);
+			
+			if (numbers.size() == 0)
+				return indexes;
 
 			return indexes;
 		}
@@ -19,12 +22,17 @@ class Solution {
 int main()
 {
 	Solution s;
+	
+	// Test case 1: Should output 1,2 i.e the indexes
 	auto rets = s.twoSum(vector<int>{ 2, 7, 11, 15 }, 9);
-
-	// Should output 1,2 i.e the indexes
 	for (auto value : rets)
 		cout << value << " ";
-		
+	
+	// Test case 2: Should return 2,5
+	rets = s.twoSum(vector<int>{8,9,14,16,21,29}, 30);
+	for (auto value : rets)
+		cout << value << " ";
+
 	cout << endl;
     return 0;
 }
