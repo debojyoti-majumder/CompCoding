e25d7b9a74a2c44897aaaa1d08a45566decdd1ec
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-07-26 13:37:52 +0530

    Adding test cases
    Solution to be added.

---
+++ july2017/addbin.cpp
@@ -0,0 +1,27 @@
+// addBin.cpp : https://leetcode.com/problems/add-binary/#/description
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+	public:
+		string addBinary(string a, string b) {
+			string retString("");
+
+			return retString;
+	}
+};
+
+int main()
+{
+	Solution s;
+	// Should out put "100"
+	cout << s.addBinary("11", "1") << endl;
+    
+	cout << s.addBinary("11000011", "01") << endl;
+	return 0;
+}
