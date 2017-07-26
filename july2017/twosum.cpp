23123db49c4278bc436a1370c1606e8ccf08b665
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-07-26 12:54:29 +0530

    Functionally correct but time limit error
    Might need to write own find method

--- july2017/twosum.cpp
+++ july2017/twosum.cpp
@@ -3,6 +3,7 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,27 +12,22 @@ class Solution {
 		vector<int> twoSum(vector<int>& numbers, int target) {
 			vector<int> indexes(2);
 			size_t sz = numbers.size();
-			size_t start_point = numbers.size();
+			size_t start_point = sz - 1;
 
 			if (sz == 0)
 				return indexes;
 
-			for (size_t i = sz; i > 0; i--) {
-				if (numbers[i] < target) {
-					start_point = i;
-					break;
-				}
-			}
-
 			while (start_point != 0) {
-				// Looking for possible pairs
-				for (size_t m = start_point - 1; m >= 0; m--) {
-					// Checking for the sum and updating the index
-					if (numbers[m] + numbers[start_point] == target) {
-						indexes[0] = m + 1;
-						indexes[1] = start_point;
-						return indexes;
-					}
+				int reminder = target - numbers[start_point];
+				auto item = find(numbers.begin(), numbers.end(), reminder);
+
+				// If the item found then the sum is found
+				if (item != numbers.end()) {
+					// Setting the indexes
+					indexes[0] = item - numbers.begin() + 1;
+					indexes[1] = start_point + 1;
+
+					return indexes;
 				}
 
 				// Going for the next big item
@@ -51,7 +47,8 @@ int main()
 	auto rets = s.twoSum(vector<int>{ 2, 7, 11, 15 }, 9);
 	for (auto value : rets)
 		cout << value << " ";
-	
+	cout << endl;
+
 	// Test case 2: Should return 2,5
 	rets = s.twoSum(vector<int>{8,9,14,16,21,29}, 30);
 	for (auto value : rets)
