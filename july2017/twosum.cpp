b7a06b2ed0ab1978f58063274e8b3a1d0890b2f9
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-07-27 23:38:47 +0530

    Making it work with unsorted array also

--- july2017/twosum.cpp
+++ july2017/twosum.cpp
@@ -1,4 +1,5 @@
 // pr1.cpp : https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+// Solves this also https://leetcode.com/problems/two-sum/#/description
 
 #include "stdafx.h"
 #include <vector>
@@ -9,7 +10,7 @@ using namespace std;
 
 class Solution {
 	public:
-		vector<int> twoSum(vector<int>& numbers, int target) {
+		vector<int> twoSumSorted(vector<int>& numbers, int target) {
 			auto high_index = numbers.size() - 1;
 			auto low_index = 0;
 
@@ -22,6 +23,12 @@ class Solution {
 
 			return vector<int>{low_index + 1, (int)(high_index + 1)};
 		}
+
+		// Yet to be implemented
+		vector<int> twoSum(vector<int>& nums, int target) {
+			sort(nums.begin(), nums.end());
+			return twoSumSorted(nums, target);
+		}
 };
 
 
@@ -30,13 +37,13 @@ int main()
 	Solution s;
 	
 	// Test case 1: Should output 1,2 i.e the indexes
-	auto rets = s.twoSum(vector<int>{ 2, 7, 11, 15 }, 9);
+	auto rets = s.twoSum(vector<int>{ 2, 11, 15, 7}, 9);
 	for (auto value : rets)
 		cout << value << " ";
 	cout << endl;
 
 	// Test case 2: Should return 2,5
-	rets = s.twoSum(vector<int>{8,9,14,16,21,29}, 30);
+	rets = s.twoSum(vector<int>{8,9,14,21,29,16}, 30);
 	for (auto value : rets)
 		cout << value << " ";
 
