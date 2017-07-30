fe7d58a0613e2b7bd5176c05c6c231485dd131e0
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-07-30 11:42:50 +0530

    Better resuability

--- LeetCode_TreeNode.h
+++ LeetCode_TreeNode.h
@@ -5,9 +5,11 @@ struct TreeNode {
 	TreeNode *left;
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 
-	// Builds a tree from the array
-	static TreeNode* buildTree(int* nums, size_t array_size) {
+struct MyTreeNode : public TreeNode {
+	// Should build the tree form a array
+	static TreeNode* buildTreeNode(int* arr, size_t sz) {
 		TreeNode* root(nullptr);
 
 		return root;
