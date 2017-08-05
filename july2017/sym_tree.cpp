6a8929077f4717d0706e820de9df9c363d5f5d61
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-08-05 23:09:33 +0530

    Problem accepted

--- july2017/sym_tree.cpp
+++ july2017/sym_tree.cpp
@@ -16,12 +16,24 @@ using namespace std;
 class Solution {
 	private:
 		bool is_mirror(TreeNode* node_1, TreeNode* node_2) {
-			return true;
+			// Base case
+			if (node_1 == nullptr && node_2 == nullptr)
+				return true;
+
+			if (node_1 != nullptr && node_2 != nullptr &&
+				node_1->val == node_2->val) {
+				bool ret_1 = is_mirror(node_1-> left, node_2->right);
+				bool ret_2 = is_mirror(node_1->right, node_2->left);
+
+				return ret_1 && ret_2;
+			}
+
+			return false;
 		}
 
 	public:
 		bool isSymmetric(TreeNode* root) {
-			return true;
+			return is_mirror(root, root);
 		}
 };
 
@@ -38,7 +50,8 @@ int main()
 	TreeNode* nd_lev2_3 = new TreeNode(4);
 	TreeNode* nd_lev2_4 = new TreeNode(3);
 
-	nd_lev1_1->left = nd_lev2_1;
+	//nd_lev1_1->left = nd_lev2_1;
+
 	nd_lev1_1->right = nd_lev2_2;
 
 	nd_lev1_2->left = nd_lev2_3;
