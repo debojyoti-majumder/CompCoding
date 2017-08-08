b7533e5f4378b175cf3248ad9598dbdc0633d608
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-08-08 23:50:23 +0530

    Simple update

--- state_machine.cpp
+++ state_machine.cpp
@@ -1,4 +1,13 @@
-class StateMachine {
+
+class StopWatchStateMachine {
+  private:
+    int counter
+  public:
+    void start();
+    void start(int data);
+    
+    void getTickcount();
+    void setTickGranularity();
 };
 
 // Throw an exception to indicate an invalid operation
