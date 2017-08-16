43d78626e2f2be839224f87adbd4c3183f2c4540
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-08-16 21:54:48 +0530

    Solution accepted

--- string_add.cpp
+++ string_add.cpp
@@ -1,22 +1,55 @@
-// Problem description: https://leetcode.com/problems/add-strings/description/
-
-#include <iostream>
 #include <string>
-
-using namespace std;
 
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        string return_value;    
+        auto num1_length = num1.length();
+        auto num2_length = num2.length();
+        string op1,op2, return_string("");
+        auto op_size = num1_length;
+
+        if( num1_length > num2_length ) {
+            op1 = num1;
+            op2="";
+
+            // Appending zeros
+            for(int i=0; i<num1_length - num2_length; i++)
+                op2 += "0";
+
+            op2 += num2;
+        }
+        else {
+            op1 = num2;
+            op2="";
+
+            // Appending zeros
+            for(int i=0; i<num2_length - num1_length; i++)
+                op2 += "0";
+
+            op2 += num1;
+        }
+
+        int carry = 0;
+
+        for( int i=op1.length()-1; i>=0; i-- ) {
+            int x = op1[i] - 48;
+            int y = op2[i] - 48;
+            
+            int add = x+y+carry;
+
+            if( add >= 10 ) {
+                carry = 1;
+                add = add - 10;
+            }
+            else
+                carry = 0;
+
+            return_string = to_string(add) + return_string;
+        }
+
+        if( carry == 1 )
+            return_string = "1" + return_string;
+
+        return return_string;
     }
 };
-
-int main() {
-    Solution s;
-
-    // Should output "356890"
-    cout << s.addStrings("2234","354656") << endl;
-
-    return 0;
-}
