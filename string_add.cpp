8331da3f872da6bf66ac779e61f120d913d618ba
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-08-16 20:36:53 +0530

    Initial code with test case

---
+++ string_add.cpp
@@ -0,0 +1,22 @@
+// Problem description: https://leetcode.com/problems/add-strings/description/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string return_value;    
+    }
+};
+
+int main() {
+    Solution s;
+
+    // Should output "356890"
+    cout << s.addStrings("2234","354656") << endl;
+
+    return 0;
+}
