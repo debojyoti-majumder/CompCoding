d77cb329de0982f3cb07b984f823271167ebf9a0
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-08-17 23:18:46 +0530

    Adding C++11 lambda function
    Need to look in the async stuff later

--- modern_cpp/lambda_1.cpp
+++ modern_cpp/lambda_1.cpp
@@ -14,6 +14,10 @@ void templated_function(T x) {
     cout << "Final step" << endl;
 }
 
+void proper_template_function(function<int(string)> func) {
+    cout << "Int value from string :" << func("hello world") << endl;
+}
+
 // This is older way of specifing callbacks
 class MyFunctor {
     int x = 0;          // C++11 speciifc
@@ -26,6 +30,21 @@ public:
 int main() {
     MyFunctor ftor;
 
+    // Older way of invoking a callback
     templated_function(ftor);
+
+    // Lambdas can be passed to the same templated function
+    templated_function([](){
+        cout << "Un named function" << endl;
+    });
+
+    proper_template_function([](string m){
+        return m.length() - 1;
+    });
+
+    proper_template_function([](string m){
+        return 0;
+    });
+
     return 0;
 }
