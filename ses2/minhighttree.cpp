0c1984e5ed34a55429c42d8e6bef3d02906ea4b8
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-09-27 19:36:15 +0530

    Initial approach yet to be tested

--- ses2/minhighttree.cpp
+++ ses2/minhighttree.cpp
@@ -1,9 +1,61 @@
 #include <vector>
 
+class TreeNode {
+private:
+	int _nodeid;
+	int _depth;
+	vector<TreeNode> _childrens;
+
+public:
+	TreeNode(int id) : _nodeid(id), _depth(0), _childrens({}) {}
+	
+	void addChild(TreeNode nd) {
+		if (_depth == 0)
+			_depth = 1;
+
+		_childrens.push_back(nd);
+		for (auto child : _childrens)
+			child.increaseDepth();
+	}
+
+	void increaseDepth() {
+		_depth += 1;
+		for (auto child : _childrens)
+			child.increaseDepth();
+	}
+};
+
 class Solution {
+private:
+	map<int, TreeNode> _nodeList;
+
+	// Creates and get's the node for the node list
+	TreeNode getNode(int nodeId) {
+		auto n = _nodeList.find(nodeId);
+		if (n == end(_nodeList)) {
+			TreeNode m(nodeId);
+
+			_nodeList.insert({ nodeId, m });
+			return m;
+		}
+		else
+			return n->second;
+	}
+
 public:
 	vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-		return {  1 };
+		for (auto edge : edges) {
+			if (edge.first > n || edge.second > n)
+				throw "Invalid Edge input exception";
+
+			auto nd_1 = getNode(edge.first);
+			auto nd_2 = getNode(edge.second);
+
+			nd_1.addChild(nd_2);
+			nd_2.addChild(nd_1);
+		}
+
+		return { 1 };
 	}
 };
 
@@ -25,3 +77,4 @@ void testMindepth() {
 	cout << endl;
 
 }
+
