c4ccf514cf30ed7682acb23b752d7142b16f65ec
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-11-08 08:38:44 +0530

    Don't need that many functions

--- ses2/game_of_life.hpp
+++ ses2/game_of_life.hpp
@@ -1,5 +1,3 @@
-// Leetcode url: https://leetcode.com/problems/game-of-life/description/
-
 struct Position {
     int x;
     int y;
@@ -42,89 +40,23 @@ private:
         auto count = 0;
         
         for( auto cell : cells ) {
-            if( _board[cell.x][cell.y] == 1 )
+            if( _board[cell.y][cell.x] == 1 )
                 count++;
         }
         
         return count; 
     }
     
-    bool checkForRule1(struct Position p) {
-        if( _board[p.x][p.y] == 1 ) {
-            auto live_count = getLiveCount(getNeighbors(p));
-        
-            if( live_count < 2 ) {
-                _tempboard[p.x][p.y] = 0;
-                return true;
-            }
-            else {
-                _tempboard[p.x][p.y] = 1;
-                return false;
-            }
-        }
-        
-        return false;
-    }
-    
-    bool checkForRule2(struct Position p) {
-        if( _board[p.x][p.y] == 1 ) {
-            auto live_count = getLiveCount(getNeighbors(p));
-            
-            if( live_count == 2 || live_count == 3 ) {
-                _tempboard[p.x][p.y] = 1;
-                return true;
-            }
-        }
-        
-        _tempboard[p.x][p.y] = 0;
-        return false;
-    }
-    
-    bool checkForRule3(struct Position p) {
-        if( _board[p.x][p.y] == 1 ) {
-            auto live_count = getLiveCount(getNeighbors(p));
-            if( live_count > 3 ) {
-                _tempboard[p.x][p.y] = 0;
-                return true;
-            }
-        }
-        
-        _tempboard[p.x][p.y] = 0;
-        return false;
-    }
-    
-    bool checkForRule4(struct Position p) {
-        if( _board[p.x][p.y] == 0 ) {
-            auto live_count = getLiveCount(getNeighbors(p));
-            if( live_count == 3 ) {
-                _board[p.x][p.y] == 1;
-                return true;
-            }   
-        }
-        
-        return false;
-    }
-    
 public:
     void gameOfLife(vector<vector<int>>& board) {
         _board = board;
-        _tempboard = board;
         
-        for( auto i=0; i<board.size(); i++ ) {
-            for( auto j=0; j<board[i].size(); j++ ) {
-                auto res = checkForRule1({i,j});
-                if( res )   continue;
-                
-                res = checkForRule2({i,j});
-                if( res )   continue;
-                
-                res = checkForRule3({i,j});
-                if( res )   continue;
-                
-                checkForRule4({i,j});
+        for(auto i=0; i<board.size(); i++) {
+            for(auto j=0; j<board[i].size(); i++) {
+                auto live_count = getLiveCount(getNeighbors({i,j}));
             }
         }
-        
-        board = _tempboard;
+
+        board = _board;
     }
 };
