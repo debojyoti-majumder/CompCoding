d59cb8965c741e16e71993be63982737ab49edba
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-11-08 13:04:37 +0530

    Partial implemtation

--- ses2/zero_mat.py
+++ ses2/zero_mat.py
@@ -1,17 +1,50 @@
 # Problem URL: https://leetcode.com/problems/set-matrix-zeroes/description/
 
 class Solution:
+    def checkForAllOnes(self, row):
+        return_value = True
+
+        for i in range(0,len(row)):
+            if row[i] == 0:
+                return_value = False
+                break;
+        
+        return return_value
+
     def setZeroes(self, matrix):
-        print "Calling set zero function"
         """
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
 
+        allOnesRows = []
+        zeroRows = []
+
+        for i in range(0,len(matrix)):
+            if self.checkForAllOnes(matrix[i]):
+                allOnesRows.append(i)
+            else:
+                zeroRows.append(i)
+        
+        # If there rows with all ones apply column rule
+        if len(allOnesRows):
+            idx = allOnesRows[0];
+            
+        # Set the zero rows
+        for r in zeroRows:
+            tar_row = matrix[r]
+            for i in range(0,len(tar_row)):
+                tar_row[i] = 0
+
+
+
 def main():
     sol1 = Solution()
-    test1 = [[0,1,1,1],[1,0,1,1],[1,1,1,1],[1,1,1,1]]  
+    test1 = [[1,1,1,1],[0,1,1,1],[1,0,1,1],[1,1,1,1]]
+
     sol1.setZeroes(test1) # Expected output: [[0,0,0,0],[0,0,0,0],[0,0,1,1],[0,0,1,1]]
+
+    print test1
 
 if __name__ == "__main__":
     main()
