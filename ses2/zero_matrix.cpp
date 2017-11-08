cba0f3e272c33608eef690620b3192e72ab519f5
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-11-08 10:15:23 +0530

    Template file

---
+++ ses2/zero_matrix.cpp
@@ -0,0 +1,18 @@
+// Problem URL: https://leetcode.com/problems/set-matrix-zeroes/description/
+// Look for a row with all 1's do a column wise scan starting from that item
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
