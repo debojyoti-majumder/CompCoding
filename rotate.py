56baa20103457c51afe49f39b50e326e998beeb7
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-11-10 18:18:43 +0530

    Creating template file

---
+++ rotate.py
@@ -0,0 +1,14 @@
+# Problem URL: https://leetcode.com/problems/rotate-image/description/
+
+class Solution:
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        
+def main():
+    print "Main function"
+
+if __name__ == "__main__":
+    main()
