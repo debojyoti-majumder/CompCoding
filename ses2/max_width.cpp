ff5ea584d86285ff6a52be1c75968f0a014fe8c6
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-12-12 16:45:22 +0530

    Still wrong but better code

--- ses2/max_width.cpp
+++ ses2/max_width.cpp
@@ -1,5 +1,4 @@
-// Problem URL:https://leetcode.com/problems/maximum-width-of-binary-tree/description/
-/**
+/** https://leetcode.com/problems/maximum-width-of-binary-tree/description/
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -10,24 +9,31 @@
  */
 class Solution {
 private:
-    int _count;
-    
-    void recurserMethod(TreeNode* nd, int depth) {
-        if( nd == nullptr ) {
-            _count += depth;
-            return;
-        }
-        
-        int n_depth = depth + 1;
-        recurserMethod(nd->left, n_depth);
-        recurserMethod(nd->right, n_depth);
-    }
-    
+	bool isLeafNode(TreeNode* nd) {
+		if (nd == nullptr)
+			return true;
+
+		if (nd->left == nullptr && nd->right == nullptr)
+			return true;
+		else
+			return false;
+	}
+
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-        _count = 0;
-        recurserMethod(root,0);
-        
-        return _count;
-    }
+	int widthOfBinaryTree(TreeNode* root) {
+		if (root == nullptr) {
+			return 0;
+		}
+
+		if (isLeafNode(root))
+			return 1;
+
+		auto l_width = widthOfBinaryTree(root->left);
+		auto r_width = widthOfBinaryTree(root->right);
+
+		if (l_width != r_width)
+			return max({ l_width,r_width });
+		else
+			return l_width + r_width;
+	}
 };
