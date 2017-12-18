07ae7aff6e7f827df9adad3c8bf915e76a816f3f
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-12-18 17:40:55 +0530

    Adding the failed test case.
    Added the failed test case in the main, no change in the main algorithm

--- ses2/max_width.cpp
+++ ses2/max_width.cpp
@@ -1,3 +1,24 @@
+// ConsoleApplication4.cpp : Finds the maximum depth of the tree
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+// Problem URL:https://leetcode.com/problems/maximum-width-of-binary-tree/description/
+ 
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 private:
 	map<int, vector<int>> _depToPosVector;
@@ -38,3 +59,34 @@ public:
 		return *ret_it;
 	}
 };
+
+int main()
+{
+	TreeNode* tree1 = new TreeNode(1);
+
+	Solution s;
+
+	tree1->left = new TreeNode(2);
+	tree1->left->left = new TreeNode(4);
+	tree1->left->right = new TreeNode(5);
+
+	tree1->right = new TreeNode(3);
+	//tree1->right->left = new TreeNode(6);
+	//tree1->right->right = new TreeNode(7);
+
+	// Should output 2 
+	//cout << s.widthOfBinaryTree(tree1) << endl;
+
+	TreeNode* tree2 = new TreeNode(10);
+	tree2->left = new TreeNode(20);
+	tree2->left->left = new TreeNode(30);
+	tree2->left->left->left = new TreeNode(40);
+	
+	tree2->right = new TreeNode(50);
+	tree2->right->right = new TreeNode(60);
+	tree2->right->right->right = new TreeNode(70);
+
+	// Should output 8
+	cout << s.widthOfBinaryTree(tree2) << endl;
+	return 0;
+}
