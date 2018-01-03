dafae916d1d05bf690b12f09267c57883893a50f
debojyoti-majumder <debojyoti.majumder@gmail.com>
2018-01-03 23:18:18 +0530

    Added test code and methods

--- ses2/invBinTree.cpp
+++ ses2/invBinTree.cpp
@@ -6,15 +6,54 @@ struct TreeNode {
      TreeNode *left;
      TreeNode *right;
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+	 void displayInOrder() {
+
+	 }
 };
 
 class Solution {
+private:
+	void invertRecurser(TreeNode* nd) {
+		if( isLeaf(nd) == false ) {
+			invertRecurser(nd->left);
+			invertRecurser(nd->right);
+		}
+	}
+
+	bool isLeaf(TreeNode* nd) const {
+		if( nd == nullptr )
+			return true;
+
+		if( nd->left == nullptr && nd->right == nullptr )
+			return true;
+		
+		return false;
+	}
+
 public:
     TreeNode* invertTree(TreeNode* root) {
-        return nullptr;
+		invertRecurser(root);
+        return root;
     }
 };
 
 int main() {
+	TreeNode* root = new TreeNode(4);
+	TreeNode* leftSubTree = new TreeNode(2);
+	TreeNode* rightSubTree = new TreeNode(3);
+
+	leftSubTree->left = new TreeNode(9);
+	leftSubTree->right = new TreeNode(5);
+
+	rightSubTree->left = new TreeNode(3);
+	rightSubTree->right = new TreeNode(8);
+
+	root->left = leftSubTree;
+	root->right = rightSubTree;
+
+	Solution s;
+	s.invertTree(root);
+	
 	return 0;
 }
