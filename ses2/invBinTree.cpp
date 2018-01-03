e3036b3f22d16304210597fc4d74d68c505b18af
debojyoti-majumder <debojyoti.majumder@gmail.com>
2018-01-03 19:10:34 +0530

    Adding skeleton code for the problem

---
+++ ses2/invBinTree.cpp
@@ -0,0 +1,20 @@
+// Problem Url: https://leetcode.com/problems/invert-binary-tree/description/
+// This code should invert a binary tree
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        return nullptr;
+    }
+};
+
+int main() {
+	return 0;
+}
