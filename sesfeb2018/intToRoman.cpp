0cd23c29c0e77d629c05d7a340b03240367c8e8e
debojyoti <debojyoti.majumder@gmail.com>
2018-02-19 22:29:05 +0530

    Initial implementation

--- sesfeb2018/intToRoman.cpp
+++ sesfeb2018/intToRoman.cpp
@@ -1,13 +1,41 @@
 // Problem URL: https://leetcode.com/problems/integer-to-roman/description/
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
+private:
+    vector<string>  _indexedStrings;        
+
 public:
+    Solution() {
+        _indexedStrings.push_back("I");
+        _indexedStrings.push_back("II");
+        _indexedStrings.push_back("III");
+        _indexedStrings.push_back("VI");
+        _indexedStrings.push_back("V");
+        _indexedStrings.push_back("VI");
+        _indexedStrings.push_back("VII");
+        _indexedStrings.push_back("VIII");
+        _indexedStrings.push_back("IX");
+    }
+
     string intToRoman(int num) {
         string romanString("");
+        auto inputNumber = num;
+
+        while( inputNumber > 0 ) {
+            if( inputNumber > 10 ) {
+                romanString += "X";
+                inputNumber -= 10;
+            }
+            else {
+                romanString += _indexedStrings[ inputNumber - 1];
+                inputNumber = 0;
+            }
+        }   
 
         return romanString;
     }
