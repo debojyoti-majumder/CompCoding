ba4ea5a6bbe437e1613eebad70f0abd999e35600
debojyoti <debojyoti.majumder@gmail.com>
2018-02-19 22:20:40 +0530

    Adding skeleton file for impl

---
+++ sesfeb2018/intToRoman.cpp
@@ -0,0 +1,25 @@
+// Problem URL: https://leetcode.com/problems/integer-to-roman/description/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string romanString("");
+
+        return romanString;
+    }
+};
+
+int main() {
+    Solution s;
+
+    // Should output VI
+    cout << s.intToRoman(6) << endl;
+
+    // Should output XXXII
+    cout << s.intToRoman(32) << endl;
+    return 0;
+}
