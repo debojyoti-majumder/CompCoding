055f3d6a4a7e3fb37dcacf22acfe7d03d6f2fe26
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-05-21 18:44:57 +0530

    Modified the algorithm

--- ses2/aoc_day3.cpp
+++ ses2/aoc_day3.cpp
@@ -1,104 +1,127 @@
-#include <vector>
+#include "stdafx.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class SpiralHelper {
-	private:
-		// Holds the current size of the squere matrix
-		unsigned int _currentSize;
-		
-		// 1 - RIGHT, 2 - TOP,3 - LEFT, 4 - BOTTOM
-		unsigned int _currentDirection;
-		
-		// Matrix to be returned
-		vector<vector<int>>	_spiralMatrix;
+class SprialHelper {
+private:
+	unsigned int _x, _y, _cellsLeft;
+	unsigned int _direction;
+	vector<vector<int>> _matrix;
+	
+	bool allocateIfNeeded() {
+		if (_cellsLeft == 0) {
+			switch (_direction)
+			{
+			case 1: {
+				for (auto& p : _matrix) {
+					p.push_back(-1);
+					_cellsLeft++;
+				}
+				_x++;
+				break;
+			}
 
-		// Matrix location cursor
-		unsigned int _x, _y;
+			case 2: {
+				vector<int> p(_matrix[_y].size());
+				_cellsLeft += p.size();
+				_matrix.insert(_matrix.begin(), p);
+				_y = 0;
+				break;
+			}
 
-		int getMinDistance(const vector<vector<int>>& matrix) {
-			return 0;
+			case 3: {
+				for (auto& p : _matrix) {
+					p.insert(p.begin(), -1);
+					_cellsLeft++;
+				}
+				_x = 0;
+				break;
+			}
+
+			case 4: {
+				vector<int> p(_matrix[_y].size());
+				_cellsLeft += p.size();
+				_matrix.push_back(p);
+				_y++;
+				break;
+			}
+
+			default:
+				break;
+			}
+			return true;
 		}
-
-		bool isFull(int currNumber) {
-			if(_x == _spiralMatrix[0].size() - 1 || _x == 0 )
-				return true;
-			if (_y == _spiralMatrix[0].size() - 1 || _y == 0)
-				return true;
-
+		else
 			return false;
+	}
+
+	void updateXY() {
+		switch (_direction) {
+		case 1:
+			_x++;
+			break;
+
+		case 2:
+			_y--;
+			break;
+
+		case 3:
+			_x--;
+			break;
+
+		case 4:
+			_y++;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	void changeDirection() {
+		_direction++;
+		if (_direction == 5)
+			_direction = 1;
+	}
+
+	void putItem(int num) {
+		auto allocated = allocateIfNeeded();
+		
+		if (allocated) {
+			_matrix[_y][_x] = num;
+			changeDirection();
+		}
+		else {
+			updateXY();
+			_matrix[_y][_x] = num;
 		}
 
-		void putItem(int num) {
-			auto toAdd = isFull(num);
-			
-			// Need to change direction
-			if (toAdd) {
-				_currentDirection += 1;
-				_currentDirection = _currentDirection % 4;
-			}
+		_cellsLeft--;
+	}
 
-			switch (_currentDirection) {
-				// Add column to right
-				case 1:
-					if (toAdd) {
-						for (auto p : _spiralMatrix)
-							p.push_back(-1);
-					}
+public:
+	SprialHelper() : _x(0), _y(0), _direction(1) {
+		vector<int> m(1);
+		m[0] = 1;
 
-					_x++;
-					break;
+		_matrix.push_back(m);
+	}
 
-				case 2: {
-					if (toAdd) {
-						vector<int> p(_spiralMatrix[0].size());
-						_spiralMatrix.insert(_spiralMatrix.begin(), p);
-					}
-
-					_y--;
-					break;
-				}
-
-				case 3:
-					if( toAdd ) {
-						for (auto p : _spiralMatrix)
-							p.insert(p.begin(), -1);
-					}
-					
-					_x--;
-					break;
-
-				case 4:{
-					if( toAdd ) {
-						vector<int> p(_spiralMatrix[0].size);
-						_spiralMatrix.push_back(p);
-					}
-
-					_y++;
-					break;
-				}
-			}
+	int getDistance(int num) {
+		// Generating the matrix
+		for (auto i = 2; i < num; i++) {
+			putItem(i);
 		}
 
-		vector<vector<int>> generateMatrix(int number) {
-			vector<int> m(1);
-
-			_spiralMatrix.clear();
-			_spiralMatrix.push_back(m);
-			
-			for (auto i = 2; i < number; i++) {
-				putItem(i);
-			}
-
-			return _spiralMatrix;
-
-		}
-
-	public:
-		SpiralHelper() : _currentDirection(0), _x(0), _y(0) {}
-
-		int getDistance(int number) {
-			return getMinDistance(generateMatrix(number));
-		}
+		return 0;
+	}
 };
+
+int main() {
+	SprialHelper h;
+	cout << h.getDistance(23) << endl;
+
+	return 0;
+}
