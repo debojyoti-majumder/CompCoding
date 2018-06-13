9c8beff0b86d823d6fbd3bfda40768dd08aa8fcc
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-06-13 14:47:19 +0530

    Causing stack overflow, Recursion causing problem
    Might have to look for an iterative solution

---
+++ ses2/leetcode/prob4.cpp
@@ -0,0 +1,33 @@
+// Problem URL: https://leetcode.com/problems/integer-replacement/description/
+
+#include "stdafx.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int integerReplacement(int n, int stepCount = 0) {
+		if (n == 1)    return stepCount;
+
+		if (n % 2 == 0) {
+			return integerReplacement(n / 2, stepCount + 1);
+		}
+		else {
+			auto d1 = integerReplacement(n - 1, stepCount + 1);
+			auto d2 = integerReplacement(n + 1, stepCount + 1);
+
+			return min(d1, d2);
+		}
+	}
+};
+
+int main() {
+	Solution s;
+	cout << s.integerReplacement(8) << endl;
+	cout << s.integerReplacement(2147483647) << endl;
+
+	return 0;
+}
+
