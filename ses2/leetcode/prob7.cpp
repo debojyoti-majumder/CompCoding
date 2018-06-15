48f8c32ae94ae372f6e0fbb85cc8f7a80c0a2681
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-06-15 20:48:30 +0530

    The problem statement is very unclear
    But got some idea

---
+++ ses2/leetcode/prob7.cpp
@@ -0,0 +1,8 @@
+// Problem URL: https://leetcode.com/problems/image-overlap/description/
+
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+      return 0;      
+    }
+};
