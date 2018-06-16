1927e23a79c51c8d778b49c7b9f6e22f542534d6
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-06-16 14:49:04 +0530

    Implementation complete
    The algorithm is correct python code worked out fine for the same algorithm

--- ses2/leetcode/prob8.cpp
+++ ses2/leetcode/prob8.cpp
@@ -2,13 +2,24 @@
 
 #include <iostream>
 #include "helperFunc.hpp"
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        return 0;        
+        vector<int> reversed(citations);
+        reverse(reversed.begin(), reversed.end());
+        int hIndex = 0;
+
+        for(auto i=0; i<=reversed.size(); i++) {
+            auto minValue = min(reversed[i], i+1);
+            if( minValue > hIndex )
+                hIndex = minValue;
+        }
+        
+        return hIndex;        
     }
 };
 
@@ -17,7 +28,17 @@ int main(int, char**) {
     Solution s;
 
     // Output should be 3
-    printVectorItems(p);
     cout << s.hIndex(p) << endl;
+
+    // Output should be 5
+    vector<int> x{3,4,5,8,10};
+    cout << s.hIndex(x) << endl;
+
+    vector<int> pp{1};
+    cout << s.hIndex(pp) << endl;
+
+    vector<int> testCase4{1,1};
+    cout << s.hIndex(testCase4) << endl;
+    
     return 0;
 }
