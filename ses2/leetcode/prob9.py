4744af8e8f5647958aacae7b1470167f358817de
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-06-18 15:59:36 +0530

    Had to see the solution
    Used recursion and some math magic

--- ses2/leetcode/prob9.py
+++ ses2/leetcode/prob9.py
@@ -1,26 +1,5 @@
-# Problem URL: https://leetcode.com/problems/super-pow/description/
-
-class Solution:
+class Solution(object):
     def superPow(self, a, b):
-        """
-        :type a: int
-        :type b: List[int]
-        :rtype: int
-        """
-
-        sum = 0
-        powerLength = len(b)
-
-        for i in range(0, powerLength):
-            sum += pow(10, powerLength - i - 1) * b[i]
-
-        retValue = 1
-        for i in range(0,sum):
-            retValue = retValue * a
-            retValue = retValue % 1337
-
-        return retValue
-
-        
-s = Solution()
-print(s.superPow(2,[1,6]))
+        if not b:
+            return 1
+        return pow(a, b.pop(), 1337)*self.superPow(pow(a, 10, 1337), b)%1337
