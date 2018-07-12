47293803762dea3388389188a0d4ef56da9f3b1f
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-12 17:42:59 +0530

    Solution accepted

--- ses2/leetcode/prob15.cpp
+++ ses2/leetcode/prob15.cpp
@@ -15,19 +15,40 @@ using namespace std;
 
 class MyCalendar {
 private:
-	vector<pair<int, int>> _bookings;
+	vector<pair<int,int>> _bookings;
+	
+	bool isInConflict(const pair<int, int>& booking1, const pair<int, int>& booking2) {
+		// For the start condition
+		if ((booking1.first >= booking2.first && booking1.first<booking2.second) || (booking1.second>booking2.first && booking1.second <= booking2.second))
+			return true;
+		if ((booking2.first >= booking1.first && booking2.first < booking1.second) || (booking2.second > booking1.first && booking2.second <= booking1.second))
+			return true;
+
+		return false;
+	}
 
 public:
 	bool book(int start, int end) {
 		auto retVal = true;
-		auto requestedDuration = end - start;
+		pair<int, int> requestedBooking(start, end);
 
-		for (auto bookingPair : _bookings) {
-			auto bookedForTime = bookingPair.second - bookingPair.first;
-			
+		// Wrong input
+		if (start > end)
+			return false;
+
+		// Base case
+		if (_bookings.size() == 0) {
+			_bookings.push_back(make_pair(start, end));
+			return true;
 		}
 
-		// This is to confirm bookings
+		for (auto bk : _bookings) {
+			if (isInConflict(bk, requestedBooking)) {
+				retVal = false;
+				break;
+			}
+		}
+
 		if (retVal)
 			_bookings.push_back(make_pair(start, end));
 
@@ -42,6 +63,15 @@ int main() {
 	cout << m.book(10, 20) << endl;		// returns true
 	cout << m.book(15, 25) << endl;		// returns false
 	cout << m.book(20, 30) << endl;		// returns true
-	
+	cout << m.book(8, 12) << endl;		// returns false
+
+	// Expected output: 1 1 0 0 1 0 1 1 1 0
+	MyCalendar c;
+	vector<pair<int, int>> inp{ {47,50 }, { 33,41 }, { 39,45 }, { 33,42 }, { 25,32 }, { 26,35 }, { 19,25 }, { 3,8 }, { 8,13 }, { 18,27 } };
+	for (auto a : inp) {
+		cout << c.book(a.first, a.second) << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
