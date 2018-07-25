29331912df15faf95f0daee7a4c9e639c85f8052
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-25 17:57:39 +0530

    Search Logic is still not good, failed test cases

--- ses2/leetcode/prob16.cpp
+++ ses2/leetcode/prob16.cpp
@@ -17,6 +17,7 @@ class Solution {
 public:
 	int ladderLength(string beginWord, string endWord, vector<string>& wordList, int currentIndex = 0) {
 		vector<int> editValues;
+		cout << beginWord << "," << endWord << currentIndex << endl;
 
 		auto it = find(wordList.begin(), wordList.end(), endWord);
 		if (it == wordList.end())
@@ -33,13 +34,23 @@ public:
 
 		currentIndex++;
 		for (auto option : editOptions) {
+			// This is the cabdidate word to find
 			auto item = beginWord;
-			item[currentIndex] = option;
+			item[currentIndex - 1] = option;
 
+			// The word needs to be in the dictinoary
+			auto foundWord = find(wordList.begin(), wordList.end(), item);
+			if (foundWord == wordList.end())
+				continue;
+			
 			int val = ladderLength(item, endWord, wordList, currentIndex);
 			if (val != 0)
-				editValues.push_back(val);
+				editValues.push_back(1 + val);
 		}
+		
+		// Means there are on paths for the current index
+		if (editValues.size() == 0)
+			return ladderLength(beginWord, endWord, wordList, currentIndex);
 
 		
 		auto val = min_element(editValues.begin(), editValues.end());
@@ -50,8 +61,11 @@ public:
 int main() {
 	Solution s1, s2;
 
-	cout << s1.ladderLength("hit", "cog", { "hot","dot","dog","lot","log","cog" });		// Should output 5
-	cout << s2.ladderLength("hit", "cog", { "hot","dot","dog","lot","log" });			// Should output 0
+	vector<string> p1{ "hot","dot","dog","lot","log","cog" };
+	vector<string> p2{ "hot","dot","dog","lot","log" };
+
+	cout << s1.ladderLength("hit", "cog", p1) << endl;			// Should output 5
+	cout << s2.ladderLength("hit", "cog", p2) << endl;			// Should output 0
 
 	return 0;
 }
