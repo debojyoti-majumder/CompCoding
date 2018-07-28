d869c773091f78a7231e0a320907942da9306db6
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-28 20:18:46 +0530

    adding some more testcases
    The solution not found still seems to be a problem to handle.

--- July2018/wordtree.cpp
+++ July2018/wordtree.cpp
@@ -30,17 +30,15 @@ public:
     int ladderLength(   string beginWord, 
                         string endWord, 
                         const vector<string>& wordList, 
-                        vector<string> solutionPath = {}) {
+                        vector<string> solutionPath = {},
+                        int statckCount = 0) {
         // Base case
         if( beginWord.compare(endWord) == 0 )
-            return 0;
-        
-        cout << "Begin:" << beginWord << " ->[";
-        for(auto m : solutionPath) {
-            cout << m << " ";
-        }
-        cout << "]" << endl;
+            return 1;
 
+        // This value is used to return the end distance value
+        statckCount++;
+        
         // If endword is not present in the list no solution is possible
         auto endItem = find(wordList.begin(), wordList.end(), endWord);
         if( endItem == wordList.end() )
@@ -56,6 +54,19 @@ public:
                 solutionCnadidates.push_back(item);
         }
 
+        cout << "Stack: " << statckCount << " " << beginWord << "[";
+        for( auto sol : solutionCnadidates ) {
+            auto it = find(solutionPath.begin(), solutionPath.end(), sol);
+            if( it == solutionPath.end() )
+                cout << sol << ",";
+        }
+        cout << "]" << endl;
+        
+
+        auto s = find(solutionCnadidates.begin(), solutionCnadidates.end(), endWord);
+        if( s != solutionCnadidates.end() ) 
+            return statckCount + 1;
+
         // Recursive call to find the cost
         for( auto sol : solutionCnadidates ) {
             auto it = find(solutionPath.begin(), solutionPath.end(), sol);
@@ -63,11 +74,12 @@ public:
             // Have to look for new solution always
             if( it == solutionPath.end() ) {
                 solutionPath.push_back(sol);
-                auto v = ladderLength(sol, endWord, wordList, solutionPath) + 1;
+                auto v = ladderLength(sol, endWord, wordList, solutionPath, statckCount);
                 editValues.push_back(v);
             }
         }
 
+        // This seems to be buggy
 		auto it = min_element(editValues.begin(), editValues.end());
         if( it != editValues.end() )
             return (*it);
@@ -77,14 +89,17 @@ public:
 };
 
 int main() {
-	Solution s1, s2, s3;
+	Solution s1, s2, s3, s4;
 	
 	vector<string> wordList{"hot","dot","dog","lot","log","cog"};
 	vector<string> wordlList2{"hot","dot","dog","lot","log"};
 	vector<string> wc3{"a", "b", "c"};
+    	vector<string> wc4{"bim", "tim", "tom", "tot","fot","fit"};
 
 	cout << s1.ladderLength("hit", "cog", wordList) << endl;		// Should output 5
 	cout << s2.ladderLength("hit", "cog", wordlList2) << endl;		// Should output 0
 	cout << s3.ladderLength("a","c", wc3) << endl;                  // Should output 2          
+    	cout << s4.ladderLength("bim","fot",wc4) << endl;               // Should output 5, test case created by me
+
 	return 0;
 }
