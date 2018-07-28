d9af75dd57f7184cc76939cc0be567b9a6748d72
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-28 23:00:23 +0530

    Still last test case failing
    Have to look into the logic some more

--- July2018/wordtree.cpp
+++ July2018/wordtree.cpp
@@ -1,5 +1,4 @@
 // Problem URL: https://leetcode.com/problems/word-ladder/description/
-// TODO: Implement bfs for this problem
 // Design a BFS engine/library to do state finding
 
 #include <iostream>
@@ -7,7 +6,9 @@
 #include <set>
 #include <algorithm>
 #include <deque>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
 
 class Solution {
@@ -15,6 +16,7 @@ private:
     deque<pair<string,int>> _searchQueue;        
     vector<string>          _wordList;
     vector<string>          _excelude;
+    vector<int>             _pos;
 
     set<string> getPossibleWords(size_t pos, const string& beginWord) {
         set<string> retValue;
@@ -29,7 +31,7 @@ private:
                     retValue.insert(tmpWord);
             }
         }
-        
+
         return retValue;
     }
 
@@ -48,7 +50,9 @@ private:
     }
 
     void addItemsToQueue(pair<string,int> queueItem, string endWord) {
-        for(auto sz=0; sz<endWord.size(); sz++ ) {
+        int i = -1;
+
+        for(auto sz : _pos ) {
             auto str = queueItem.first;
             
             // This lookfor possible solutions
@@ -63,6 +67,14 @@ private:
                     addIfNotpresent(seachItem);
                 }
             }
+            else {
+                i = sz;
+            }
+        }
+
+        if( i != -1 ) {
+            auto p = find(_pos.begin(), _pos.end(), i);
+            _pos.erase(p);
         }
     }
 
@@ -72,13 +84,16 @@ public:
         _wordList = wordList;
         _searchQueue.push_back(beginState);
 
+        for( auto sz=0; sz<endWord.size(); sz++ )
+            _pos.push_back(sz);
+
         while( !_searchQueue.empty() ) {
             auto m = _searchQueue.front();
             _excelude.push_back(m.first);
 
             if( m.first.compare(endWord) == 0 )
                 return m.second;
-
+            
             _searchQueue.pop_front();
             addItemsToQueue(m,endWord);
         }
@@ -88,8 +103,10 @@ public:
 };
 
 int main() {
-	Solution s1, s2, s3, s4, s5, s6, s7;
-	
+	Solution s1, s2, s3, s4, s5, s6, s7, s8;
+    Solution s9;
+	auto start = high_resolution_clock :: now();
+
 	vector<string> wordList{"hot","dot","dog","lot","log","cog"};
 	vector<string> wordlList2{"hot","dot","dog","lot","log"};
 	vector<string> wc3{"a", "b", "c"};
@@ -100,12 +117,22 @@ int main() {
  "to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th",
  "pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io",
  "be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-	cout << s1.ladderLength("hit", "cog", wordList) << endl;		// Should output 5
-	cout << s2.ladderLength("hit", "cog", wordlList2) << endl;		// Should output 0
-	cout << s3.ladderLength("a","c", wc3) << endl;                  // Should output 2          
-    cout << s4.ladderLength("bim","fot",wc4) << endl;               // Should output 5, test case created by me
-    cout << s5.ladderLength("hot","dog", wc5) << endl;              // Should output 0
-    cout << s6.ladderLength("abc", "xyz", wc6) << endl;
-    cout << s7.ladderLength("qa","sq", wc) << endl;                 // Getting time limit
-	return 0;
+    vector<string> bigWc {"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"};
+	vector<string> reds{"ted","tex","red","tax","tad","den","rex","pee"};
+
+    cout << s1.ladderLength("hit", "cog", wordList) << ",";		// Should output 5
+	cout << s2.ladderLength("hit", "cog", wordlList2) << ",";		// Should output 0
+	cout << s3.ladderLength("a","c", wc3) << ",";                  // Should output 2          
+    cout << s4.ladderLength("bim","fot",wc4) << ",";               // Should output 5, test case created by me
+    cout << s5.ladderLength("hot","dog", wc5) << ",";              // Should output 0
+    cout << s6.ladderLength("abc", "xyz", wc6) << ",";
+    cout << s7.ladderLength("qa","sq", wc) << ",";                 // Getting time limit
+	cout << s8.ladderLength("cet","ism", wc) << ",";
+    cout << s9.ladderLength("red","tax",reds) << endl;              // Should return 4
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << duration.count() << endl;
+    return 0;
 }
