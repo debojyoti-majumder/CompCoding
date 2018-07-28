9882f58d62a9bacea660ee1ebd12de0ed5f70b2f
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-28 15:55:35 +0530

    Added some logging
    Still needs to be dubugged

--- July2018/wordtree.cpp
+++ July2018/wordtree.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 class Solution {
 private:
-    vector<string> _solutionPath;
-
     set<string> getPossibleWords(size_t pos, const vector<string>& wordList, const string& beginWord) {
         set<string> retValue;
         
@@ -29,11 +27,23 @@ private:
     }
 
 public:
-    int ladderLength(string beginWord, string endWord, const vector<string>& wordList) {
-        cout << beginWord << "--> ";
-        
+    int ladderLength(   string beginWord, 
+                        string endWord, 
+                        const vector<string>& wordList, 
+                        vector<string> solutionPath = {}) {
         // Base case
         if( beginWord.compare(endWord) == 0 )
+            return 0;
+        
+        cout << "Begin:" << beginWord << " ->[";
+        for(auto m : solutionPath) {
+            cout << m << " ";
+        }
+        cout << "]" << endl;
+
+        // If endword is not present in the list no solution is possible
+        auto endItem = find(wordList.begin(), wordList.end(), endWord);
+        if( endItem == wordList.end() )
             return 0;
 
         vector<string> solutionCnadidates;
@@ -48,31 +58,33 @@ public:
 
         // Recursive call to find the cost
         for( auto sol : solutionCnadidates ) {
-            auto it = find(_solutionPath.begin(), _solutionPath.end(), sol);
+            auto it = find(solutionPath.begin(), solutionPath.end(), sol);
             
             // Have to look for new solution always
-            if( it == _solutionPath.end() ) {
-                _solutionPath.push_back(sol);
-                editValues.push_back(ladderLength(sol, endWord, wordList) + 1);
+            if( it == solutionPath.end() ) {
+                solutionPath.push_back(sol);
+                auto v = ladderLength(sol, endWord, wordList, solutionPath) + 1;
+                editValues.push_back(v);
             }
         }
 
 		auto it = min_element(editValues.begin(), editValues.end());
         if( it != editValues.end() )
-            return *it;
+            return (*it);
         else
             return 0;
     }
 };
 
 int main() {
-	Solution s1, s2;
+	Solution s1, s2, s3;
 	
 	vector<string> wordList{"hot","dot","dog","lot","log","cog"};
 	vector<string> wordlList2{"hot","dot","dog","lot","log"};
-	
+	vector<string> wc3{"a", "b", "c"};
+
 	cout << s1.ladderLength("hit", "cog", wordList) << endl;		// Should output 5
 	cout << s2.ladderLength("hit", "cog", wordlList2) << endl;		// Should output 0
-	
+	cout << s3.ladderLength("a","c", wc3) << endl;                  // Should output 2          
 	return 0;
 }
