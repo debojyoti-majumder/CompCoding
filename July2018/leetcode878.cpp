e31f190ddb04968bf1b6e37de7cd8f1063e7ab12
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-07-29 14:20:32 +0530

    Second test case failing, have to debug
    Also have to add the modulo logic because the problem description says so.

--- July2018/leetcode878.cpp
+++ July2018/leetcode878.cpp
@@ -16,7 +16,7 @@ public:
         auto smaller(A);
         auto bigger(B);
         _magicalNumbers.clear();
-        
+
         if( A > B ) {
             smaller = B;
             bigger = A;
@@ -25,7 +25,29 @@ public:
         if( bigger % smaller == 0 ) 
             return smaller * N;
 
-        return 0;
+        _magicalNumbers.push_back(smaller);
+        _magicalNumbers.push_back(bigger);
+
+        for( auto i=0; i<N; i++ ) {
+            auto item_1 = _magicalNumbers[i] * smaller;
+            auto item_2 = _magicalNumbers[i] * bigger;
+
+            bool isItem1Added = false;
+            if( item_1 != _magicalNumbers[i + 1] ) {
+                isItem1Added = true;
+                _magicalNumbers.push_back(item_1);
+            }
+
+            if( isItem1Added ) {
+                _magicalNumbers.push_back(item_2);
+            }
+            else {
+                if( _magicalNumbers[i + 1] != item_2 )
+                    _magicalNumbers.push_back(item_2);
+            }
+        } 
+
+        return _magicalNumbers[N -1];
     }
 };
 
