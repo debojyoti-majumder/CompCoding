32c4b5582357c2760963e1f219e88f11014bfd91
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-09-08 17:40:20 +0530

    Builds successfully

--- modern_cpp/iterator_prob.hpp
+++ modern_cpp/iterator_prob.hpp
@@ -1,95 +1,116 @@
 #include <iostream>
-#include <string> 
-template <typename T> class BinaryTree;
+#include <string>
+
+using namespace std;
+
+template <typename T> struct BinaryTree;
 
 template <typename T> struct Node {
     T value;
-    
+
     Node<T>* left = nullptr;
     Node<T>* right = nullptr;
     Node<T>* parent = nullptr;
 
     BinaryTree<T>* tree = nullptr;
 
-    explicit Node(const T& val) : value(val) {
+    explicit Node(const T& v) : value(v) {
     }
 
-    Node(const T& val, Node<T>* const l, Node<T>* const r) : value(val), left(l), right(r) {
-        left->tree = right->tree = tree;
+    Node(const T& v, Node<T>* const l, Node<T>* const r) : value(v) , left(l), right(r) {
         left->parent = right->parent = this;
+        this->left->tree = this->right->tree = tree;
     }
 
-    void setTree(BinaryTree<T>* const tr) {
-        this->tree = tr;
+    void setTree(BinaryTree<T>* p) {
+        tree = p;
 
-        // This will workout recusively
-        if( left )  this->left->setTree(tr);
-        if( right ) this->right->setTree(tr);
-    }
-};
-
-template <typename U> struct PreOrderInerator {
-    Node<U>* current;
-
-    explicit PreOrderInerator(Node<U>* current) : current(current) {
-
-    }
-
-    bool operator != (const PreOrderInerator<U>& rhs) {
-        return current != rhs.current;
-    }
-
-    Node<U>& operator*() {
-        return *current;
-    }
-
-    PreOrderInerator<U>& operator++() {
-        if( current->right ) {
-            current = current->right;
-
-            while( current->left )
-                current = current->left;
-        }
-        else {
-            auto p = current->parent;
-            while( p && current == p->right ) {
-                current = p;
-                p = p->parent;
-            }
-
-            current = p;
-        }
-
-        return *this;
+        if( left )  left->setTree(p);
+        if( right ) right->setTree(p);
     }
 };
 
-template <typename T> using iterator = PreOrderInerator<T>;
+template <typename T> struct PreOrderTraversal;
 
 template <typename T> struct BinaryTree {
     Node<T>* root = nullptr;
 
-    explicit BinaryTree(Node<T>* const r) : root{r} {
-        r->setTree(this);
+    explicit BinaryTree(Node<T>* const root) : root(root) {
+        root->setTree(this);
     }
 
-    iterator<T> begin() {
-        Node<T>* n = root;
-        if(n) {
-            while( n->left ) n = n->left;
-        }
+    PreOrderTraversal<T> begin() {
+        Node<T> *n = root;
 
-        return iterator<T>{n};
+        if (n)
+            while (n->left)
+                n = n->left;
+
+        return PreOrderTraversal<T>{n};
     }
 
-    iterator<T> end() {
-        return iterator<T>{ nullptr };
+    PreOrderTraversal<T> end() {
+        return PreOrderTraversal<T>{nullptr};
     }
 };
 
+template <typename T> struct PreOrderTraversal {
+    Node<T>* current;
+
+    explicit PreOrderTraversal(Node<T>* current) : current(current) {
+
+    }
+
+    bool operator !=(const PreOrderTraversal<T>& rhs) {
+        return current != rhs.current;
+    }
+
+    Node<T>& operator*(){
+        return *current;
+    }
+
+    PreOrderTraversal<T> &operator++() {
+        if (current->right) {
+            current = current->right;
+            while (current->left)
+                current = current->left;
+        }
+        else {
+            Node<T> *p = current->parent;
+            while (p && current == p->right) {
+                current = p;
+                p = p->parent;
+            }
+            current = p;
+        }
+        return *this;
+    }
+};
 
 int main(int argc, char const *argv[]) {
-    auto nd = new Node<string>("dad");
+    auto cdrive = new Node<string>{"Cdrive",
+        new Node<string>{"Folder1"},
+        new Node<string>{"Folder2"}
+    };
 
+    auto ddrive = new Node<string>{"Cdrive",
+        new Node<string>{"Folder1"},
+        new Node<string>{"Folder2"}
+    };
+
+    auto root = new Node<string>{"root",cdrive, ddrive};
+
+    BinaryTree<string> tree{root};
+
+    for(auto it = tree.begin(); it != tree.end(); ++it ) {
+        cout << (*it).value << " ";
+    }
+
+    // TODO: Use smart pointers later
+    delete root;
+    delete ddrive;
+    delete cdrive;
+
+    cout << endl;
     return 0;
 }
