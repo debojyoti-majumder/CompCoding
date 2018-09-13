0be99c19c2d292210bcb2b89fe6891eb6e485c6d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-09-13 16:05:03 +0530

    Not yet tested just the initial idea

---
+++ July2018/battleShip.cpp
@@ -0,0 +1,92 @@
+// Problem URL: https://leetcode.com/problems/battleships-in-a-board/description/
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+	struct Point {
+		int x;
+		int y;
+
+		Point() : x(-1), y(-1) {
+
+		}
+
+		Point(int row, int col) : x(row), y(col) {
+
+		}
+	};
+
+	using BattleShip = vector<Point>;
+	vector<BattleShip> battleShips;
+
+	bool updateBattleShip(const Point& p, BattleShip& ship) {
+		bool addPoint = false;
+
+		for (auto& point : ship) {
+			if (point.x == p.x - 1 || point.x == p.x + 1) {
+				addPoint = true;
+				break;
+			}
+
+			if (point.y == p.y - 1 || point.y == p.y + 1) {
+				addPoint = true;
+				break;
+			}
+		}
+
+		if (addPoint)
+			ship.push_back(p);
+
+		return !addPoint;
+	}
+
+	void addPointToBattleShips(const Point& p) {
+		auto isNewBattelShip = true;
+
+		for (auto& b : battleShips) {
+			isNewBattelShip = !updateBattleShip(p, b);
+		}
+
+		if (isNewBattelShip) {
+			BattleShip b{ p };
+			battleShips.push_back(b);
+		}
+	}
+
+public:
+	int countBattleships(vector<vector<char>>& board) {
+		if (board.size() == 0)
+			return 0;
+
+		vector<Point> xPoints;
+		size_t row = board.size();
+		size_t col = board[0].size();
+		
+		for (size_t i = 0; i < row; i++) {
+			for (size_t j = 0; j < col; j++) {
+				if (board[i][j] == 'X')
+					xPoints.push_back(Point{ (int)i,(int)j });
+			}
+		}
+
+		for (auto& p : xPoints) {
+			addPointToBattleShips(p);
+		}
+
+		return xPoints.size();
+	}
+};
+
+int main() {
+	vector<vector<char>> bd{ {'X', '.', '.', 'X'}, { '.', '.', '.', 'X'}, { '.', '.', '.', 'X' }};
+	Solution s;
+	
+	// Should output 2
+	cout << s.countBattleships(bd) << endl; 
+
+	return 0;
+}
