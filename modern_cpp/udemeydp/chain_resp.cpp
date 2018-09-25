977be3230e58e9023b4d823331dd008501fde62d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-09-25 22:33:12 +0530

    Fixed the bug works fine now
    This example has to chain continuation and chain break. Also made use the command does not gets executed multiple times which had been happening in the previous example

--- modern_cpp/udemeydp/chain_resp.cpp
+++ modern_cpp/udemeydp/chain_resp.cpp
@@ -1,102 +1,115 @@
-// This seems to be a buggy impl, have to fix it later
+// For this command situation where this chain needs craeting multiple times for the same 
+// way but with different commands might be use full to use prototype pattern
+
+#include "stdafx.h"
 #include <iostream>
-#include <fstream>
-#include <map>
 #include <string>
-#include <chrono>
-#include <ctime>
 
 using namespace std;
 
 class DeviceCommand {
-    private:
-        string _targetIP;
-        string _commandString;
+	private:
+		string _targetIp;
+		string _command;
+	public:
+		DeviceCommand() = delete;
+		
+		// TODO: Parse these parameters
+		DeviceCommand(string ip, string command) {
+			_command = command;
+			_targetIp = ip;
+		}
 
-    public:
-        DeviceCommand() = delete;
-        DeviceCommand(const string& str, string name) : _targetIP(str) , _commandString(name) {
-        };
-        virtual ~DeviceCommand() = default;
+		void execute() {
+			cout << " Executing at " << _targetIp << " " << _command << endl;
+		}
 
-        string getDeviceHandle() {
-            return _targetIP + "->" + _commandString;
-        }
-
-        inline string getTarget() const { return _commandString; }
+		inline string getIp() const { return _targetIp; }
+		inline string getCommand() const { return _command; }
 };
 
-class SimpleExcuteAction {
-    private:
-        SimpleExcuteAction* next;
+class NoActionResponsibility {
+	private:
+		NoActionResponsibility* next;
 
-    protected:
-        DeviceCommand& _command;
+	protected:
+		DeviceCommand & _command;
 
-    public:
-        SimpleExcuteAction() = delete;
-        SimpleExcuteAction(DeviceCommand& comm) : _command(comm) , next(nullptr) {
-        }
+	public:
+		NoActionResponsibility() = delete;
+		NoActionResponsibility(DeviceCommand& command) : _command(command) {
+			next = nullptr;
+		}
+	
+		virtual void handle() {
+			if (next)	
+				next->handle();
+			else
+				_command.execute();
+		}
 
-        virtual void handle() {
-            if( next ) next->handle();
-            cout << "Executing command on " << _command.getDeviceHandle() << endl;
-        }
+		void addResponsibility(NoActionResponsibility* action) {
+			if (next)
+				next->addResponsibility(action);
+			else
+				next = action;
+		}
 
-        void addResponsibility(SimpleExcuteAction* action) {
-            if( next )
-                next->addResponsibility(action);
-            else
-                next = action;
-        }
+		virtual ~NoActionResponsibility() {
+
+		}
 };
 
-class AuditLogging : public SimpleExcuteAction {
-    public:
-        AuditLogging() = delete;
-        AuditLogging(DeviceCommand c) : SimpleExcuteAction(c) {
+class LoggingAction : public NoActionResponsibility {
+	public:
+		LoggingAction() = delete;
+		
+		LoggingAction(DeviceCommand& comm) : NoActionResponsibility(comm) {
 
-        }
+		}
 
-        void handle() override {
-            auto now = chrono::system_clock::now();
-            std::time_t t = chrono::system_clock::to_time_t(now);
-            std::cout << std::ctime(&t) << " ";
-
-            SimpleExcuteAction::handle();
-        }
+		void handle() override {
+			cout << "Command executed at this time:";
+			
+			// This function enables the flow of command to continue
+			NoActionResponsibility::handle();
+		}
 };
 
-class CommandPermission : public SimpleExcuteAction {
-    public:
-        CommandPermission() = delete;
-        CommandPermission(DeviceCommand& c) : SimpleExcuteAction(c) {
+class SecurityAction : public NoActionResponsibility {
+	public:	
+		SecurityAction() = delete;
 
-        }
+		SecurityAction(DeviceCommand& comm) : NoActionResponsibility(comm) {
+		}
 
-        void handle() override {
-            if( _command.getTarget() == "localhost" ) {
-                cout << "Not allowed";
-            }
-
-            SimpleExcuteAction::handle();
-        }
+		void handle() override {
+			// Not calling the base method breaks the chain
+			if (_command.getCommand() == "logout") {
+				cout << "Access denied" << endl;
+			}
+			else {
+				// The chain continues
+				cout << "Permission Grated" << endl;
+				NoActionResponsibility::handle();
+			}
+		}
 };
 
 int main() {
-    DeviceCommand c1("localhost", "login");
-    DeviceCommand c2("10.23.33.27", "login");
+	string inpCmd;
+	cin >> inpCmd;
 
-    auto action1 = new SimpleExcuteAction(c1);
-    auto action2 = new SimpleExcuteAction(c2);
+	DeviceCommand c1{ "localhost", inpCmd };
+	NoActionResponsibility r1(c1);
+	LoggingAction r2(c1);
+	SecurityAction r3(c1);
 
-    action1->addResponsibility(new AuditLogging(c1));
-    action1->addResponsibility(new CommandPermission(c1));
+	r1.addResponsibility(&r3);			// The security starts first
+	r1.addResponsibility(&r2);			// The the auditing
 
-    action2->addResponsibility(new AuditLogging(c2));
-    action2->addResponsibility(new CommandPermission(c2));
-
-    action1->handle();
-    action2->handle();
-    return 0;
+	// The chain starts from the top
+	r1.handle();
+	
+	return 0;
 }
