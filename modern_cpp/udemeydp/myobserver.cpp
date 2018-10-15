8c298e1373aaaf11f021b3066f30dedeca99d96b
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-10-15 13:05:55 +0530

    Generic observer with template

--- modern_cpp/udemeydp/myobserver.cpp
+++ modern_cpp/udemeydp/myobserver.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <functional>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+template <typename T>
 class DataItem {
     private:
-        int _value;
-        vector<function<void(int)>> _subscribers;
+        T& _value;
+        vector<function<void(T)>> _subscribers;
 
     public:
         DataItem() = delete;
 
-        explicit DataItem(int b) : _value(b) {}
+        explicit DataItem(T& b) : _value(b) {}
 
-        DataItem& operator=(int v) {
+        DataItem& operator=(T v) {
             if( _value != v ) {
                 cout << "Data item changed. \n";
                 _value = v;
@@ -27,13 +29,14 @@ class DataItem {
             return *this;
         }
 
-        void addSubScriber(const function<void(int)>& fnc) {
+        void addSubScriber(const function<void(T)>& fnc) {
             _subscribers.emplace_back(fnc);
         }        
 };
 
 int main() {
-    DataItem item(0);
+    int i=0;
+    DataItem<int> item(i);
 
     // setup change detetor;
     item.addSubScriber([] (int v) {
@@ -50,6 +53,14 @@ int main() {
     item = 10;
     item = 10;
     item = 30;
-    
+
+    string stringData("Debojyoti");
+    DataItem<string> stringItem(stringData);
+
+    stringItem.addSubScriber([](string v) {
+        cout << "String changed:" << v << "\n";
+    });
+
+    stringItem = "ABCD";
     return 0;
 }
