7e327b3165b0b875b9039670e40382aac80b4fb7
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-11-08 13:53:42 +0530

    Solution accepted

--- yearEnd2018/leetcode89.cpp
+++ yearEnd2018/leetcode89.cpp
@@ -11,20 +11,35 @@ using namespace std;
 class Solution {
 public:
 	vector<int> grayCode(int n) {
+		// These are base cases
 		if (n == 0)
 			return vector<int>{0};
 		else if (n == 1)
-			return vector<int>{1, 0};
+			return vector<int>{0, 1};
 
 		auto codes{ grayCode(n - 1) };
 		vector<int> retValues;
+
+		// Appending zeros
+		for (auto it = codes.cbegin(); it != codes.cend(); ++it) {
+			bitset<32> bitrepresentedNumber(*it);
+			bitrepresentedNumber.reset(n-1);
+			retValues.push_back(bitrepresentedNumber.to_ulong());
+		}
+
+		// Appending one but in reverse order
+		for (auto it = codes.crbegin(); it != codes.crend(); ++it) {
+			bitset<32> bitrepresentedNumber(*it);
+			bitrepresentedNumber.set(n-1);
+			retValues.push_back(bitrepresentedNumber.to_ulong());
+		}
 
 		return retValues;
 	}
 };
 
 template<typename T>
-void printIteratbale(T items) {
+void printIteratbale(const T& items) {
 	for (const auto& ref : items)
 		cout << ref << " ";
 }
