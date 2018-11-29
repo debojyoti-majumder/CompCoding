c705dc7204b78a085a1631ddb242d8f5d2bcbde7
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-11-29 17:06:14 +0530

    Initial commit

---
+++ yearEnd2018/leetCode846.cpp
@@ -0,0 +1,39 @@
+// Problem URL: https://leetcode.com/problems/hand-of-straights/
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+	void addItemToSubArray(vector<int>& sub, int i) {
+		if (sub.empty()) {
+			sub.emplace_back(i);
+			return;
+		}
+
+		auto it = upper_bound(sub.begin(), sub.end(), i);
+		sub.insert(it, i);
+	}
+
+public:
+	bool isNStraightHand(vector<int>& hand, int W) {
+		vector<vector<int>> handArrangment(W);
+
+		return true;
+	}
+};
+
+int main() {
+	vector<int> input1{ 1,2,3,6,2,3,4,7,8 };
+	vector<int> input2{ 1,2,3,4,5 };
+	Solution s;
+
+	cout << s.isNStraightHand(input1, 3) << endl;	// Should return true
+	cout << s.isNStraightHand(input2, 4) << endl;	// Should return false
+
+	return 0;
+}
