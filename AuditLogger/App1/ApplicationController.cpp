a2a10a1dd9d27be31e96b23460ccd4d81320042f
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2018-12-14 00:13:45 +0530

    Added application controller object

---
+++ AuditLogger/App1/ApplicationController.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "ApplicationController.h"
+#include "EventSource.h"
+#include "Poco/Observer.h"
+
+std::shared_ptr<ApplicationController> ApplicationController::_instance = nullptr;
+std::mutex ApplicationController::_instanceMutex;
+
+ApplicationController::ApplicationController() {
+	_eventSources.emplace_back(std::thread{ EventSource{_applicationNotificationCenter} });
+	_eventSources.emplace_back(thread{ EventSource{_applicationNotificationCenter, 30, "USB"} });
+	_eventSources.emplace_back(thread{ EventSource{_applicationNotificationCenter, 20, "GPIO"} });
+
+	addObservers();
+};
+
+void ApplicationController::addObservers() {
+	// Subscribing to the HW events
+	_applicationNotificationCenter.addObserver(Poco::Observer<HwEventHandler, USBEventNotfication>
+		(evtHandler, &HwEventHandler::handleUSBEvent));
+	_applicationNotificationCenter.addObserver(Poco::Observer<HwEventHandler, GPIOEventNotification>
+		(evtHandler, &HwEventHandler::handleGPIOEvent));
+}
+
+void ApplicationController::removeObservers() {
+	_applicationNotificationCenter.removeObserver(Poco::Observer<HwEventHandler, USBEventNotfication>
+		(evtHandler, &HwEventHandler::handleUSBEvent));
+	_applicationNotificationCenter.removeObserver(Poco::Observer<HwEventHandler, GPIOEventNotification>
+		(evtHandler, &HwEventHandler::handleGPIOEvent));
+}
+
+std::shared_ptr < ApplicationController > ApplicationController::getInstance() {
+	std::lock_guard<std::mutex> lockGuard{ _instanceMutex };
+	if (_instance == nullptr)
+		_instance = std::shared_ptr<ApplicationController>(new ApplicationController());
+
+	return _instance;
+}
+
+void ApplicationController::run() {
+	// Waiting for all the threads to complete
+	for (auto& t : _eventSources)
+		t.join();
+}
+
+ApplicationController::~ApplicationController() {
+	removeObservers();
+	std::cout << "Exiting controller" << std::endl;
+}
---
+++ AuditLogger/App1/ApplicationController.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <vector>
+#include <thread>
+
+#include "Poco/NotificationCenter.h"
+#include "HwEventHandler.h"
+
+class ApplicationController {
+private:
+	static std::shared_ptr<ApplicationController> _instance;
+	static std::mutex _instanceMutex;
+
+	std::vector<std::thread> _eventSources;
+	Poco::NotificationCenter _applicationNotificationCenter;
+	HwEventHandler evtHandler;
+
+	ApplicationController();
+	void addObservers();
+	void removeObservers();
+
+public:
+	static std::shared_ptr < ApplicationController > getInstance();
+	void run();
+	~ApplicationController();
+};
