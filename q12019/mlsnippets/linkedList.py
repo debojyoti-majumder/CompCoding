f17c24fd5d778060443031ac2f7f8c1914bf59fc
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-01-27 21:07:04 +0530

    Basic linked list in python

---
+++ q12019/mlsnippets/linkedList.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+    def setNext(self, nextNode):
+        self.next = nextNode
+
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = Node(head)
+
+    def addData(self, data):
+        if self.head != None:
+            iterator = self.head
+
+            # Navigate to the last node
+            while iterator.getNext() != None :
+                iterator = iterator.getNext()
+            
+            iterator.next = Node(data)
+        else:
+            self.head = Node(data)
+
+    def printAll(self):
+        it = self.head
+
+        while it != None:
+            print(it.getData())
+            it = it.getNext()
+
+    def 
+
+
+print("Testing out linked list")
+myList = LinkedList(40)
+
+myList.addData(23)
+myList.addData(45)
+myList.addData(34)
+
+myList.printAll()
