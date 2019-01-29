b65968004c18762ef7c994f30be49030285525ce
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-01-29 13:46:55 +0530

    Implemented basic Markov chain

--- q12019/mlsnippets/markov/chap1.py
+++ q12019/mlsnippets/markov/chap1.py
@@ -22,6 +22,13 @@ class EqualProbProcess:
 
         return serise
 
+def testEqualProb():
+    ps = EqualProbProcess(["head", "tail"])
+    print(ps.getStates())
+
+    ps = EqualProbProcess(["1", "2","3","4","5","6"])
+    print(ps.getStates(20))
+
 class MarkovProcess:
     # The first row represents transition probality from the state
     def __init__(self, states, transitionMatrix=[]):
@@ -36,32 +43,38 @@ class MarkovProcess:
 
         self.states = states
         self.currentStateIndex = 0
+        self.transitionMatrix = transitionMatrix
 
     def setCurrentState(self, state):
         return None
 
-    def nextState(self):
-        return None
+    def nextState(self, state):
+        i = self.states.index(state)
+
+        if i != -1:
+            probArray = self.transitionMatrix[i]
+            newState = np.random.choice(self.states, p=probArray)
+
+            self.currentStateIndex = self.states.index(newState)
+            return newState
+        else:
+            return None
 
     def getStates(self, num=10):
         chain = []
 
         for i in range(num):
-            chain.append(self.nextState())
+            currState = self.states[self.currentStateIndex]
+            chain.append(self.nextState(currState))
         
         return chain
 
-transitionMatrix = [    
+transmat = [    
     [0.8, 0.19, 0.01],
     [0.2,  0.7,  0.1],
     [0.1,  0.2,  0.7]
 ]
 
 
-mkp = MarkovProcess(["Sunny", "Rainy", "Foggy"], transitionMatrix=transitionMatrix)
-
-ps = EqualProbProcess(["head", "tail"])
-print(ps.getStates())
-
-ps = EqualProbProcess(["1", "2","3","4","5","6"])
-print(ps.getStates(20))
+mkp = MarkovProcess(["Sunny", "Rainy", "Foggy"], transitionMatrix=transmat)
+print(mkp.getStates())
