cd43aa42879dde7a67392abb9e9071f0a3d5877b
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-01-31 18:27:13 +0530

    #13 Initial commit with test input

---
+++ q12019/cppWorkspace/leetcode817.hpp
@@ -0,0 +1,38 @@
+// Problem URL: https://leetcode.com/problems/linked-list-components/
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	int numComponents(ListNode* head, vector<int>& G) {
+		return 0;
+	}
+};
+
+int testmain() {
+	ListNode* head = new ListNode(0);
+	auto it = head;
+
+	for (int i = 1; i < 4; i++) {
+		it->next = new ListNode(i);
+		it = it->next;
+	}
+
+	Solution s;
+	vector<int> links{ 0,3,1,4 };
+
+	cout << s.numComponents(head, links) << endl;
+	
+	// TODO: Memory freeing although not part of my problem
+	return 0;
+}
