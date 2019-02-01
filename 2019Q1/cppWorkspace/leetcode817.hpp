cbb639ed9cf71c566ae3f57d26db3a93e13a8b16
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-02-01 18:32:11 +0530

    Test case failure for node with single node
    Have to be debugged later

--- 2019Q1/cppWorkspace/leetcode817.hpp
+++ 2019Q1/cppWorkspace/leetcode817.hpp
@@ -9,21 +9,57 @@ using namespace std;
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
+	ListNode(int x) : val(x), next(nullptr) {}
 };
 
 class Solution {
 public:
 	int numComponents(ListNode* head, vector<int>& G) {
-		return 0;
+		auto listComponent{ G };
+		auto it = head;
+		int componentCount = 0;
+
+		while (it != nullptr) {
+			auto val = it->val;
+
+			if (it->next != nullptr) {
+				auto nextVal = it->next->val;
+
+				auto element1 = find(listComponent.begin(), listComponent.end(), val);
+				auto element2 = find(listComponent.begin(), listComponent.end(), nextVal);
+
+				// If both the element is found then we have got a component
+				if (element1 != listComponent.end() && element2 != listComponent.end()) {
+					// Adding to the component count
+					componentCount++;
+
+					// Erasing those two elements
+					listComponent[element1 - listComponent.begin()] = -1;
+					listComponent[element2 - listComponent.begin()] = -1;
+					
+					// This should double icrement
+					it = it->next;
+				}
+			}
+
+			it = it->next;
+		}
+
+		auto deletedCount = count_if(listComponent.begin(), listComponent.end(), [](int item) {
+			return item == -1;
+		});
+
+		if (deletedCount == listComponent.size() - 1)	componentCount++;
+
+		return componentCount;
 	}
 };
 
-void testSolution() {
+int main() {
 	ListNode* head = new ListNode(0);
 	auto it = head;
 
-	for (int i = 1; i < 4; i++) {
+	for (int i = 1; i <= 4; i++) {
 		it->next = new ListNode(i);
 		it = it->next;
 	}
@@ -31,5 +67,9 @@ void testSolution() {
 	Solution s;
 	vector<int> links{ 0,3,1,4 };
 
+	// Should output 2
 	cout << s.numComponents(head, links) << endl;
+	
+	// TODO: Memory freeing although not part of my problem
+	return 0;
 }
