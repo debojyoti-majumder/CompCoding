b1353b6238f62aa34a3ae292867ce88cf8c40776
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-02-11 22:50:18 +0530

    Initial commit for the problem

---
+++ 2019Q1/cppWorkspace/birthdayChok.hpp
@@ -0,0 +1,9 @@
+// Problem URL: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+#include <vector>
+
+using namespace std;
+
+int birthday(vector<int> s, int d, int m) {
+  return 0;
+}
