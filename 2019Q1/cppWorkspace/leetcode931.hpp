e115d653591f6ace5856f8fa7c40c8ecac2d9adb
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-02-22 22:30:31 +0530

    #9 Taken a new approach, but even older test cases are failing
    I think there would be a problem in the path finding, in this case the test case 3 and test case 4 is failing

--- 2019Q1/cppWorkspace/leetcode931.hpp
+++ 2019Q1/cppWorkspace/leetcode931.hpp
@@ -6,47 +6,74 @@
 
 using namespace std;
 
+typedef pair<size_t, size_t> MatrixLocation;
+
 class Solution {
 private:
 	vector<vector<int>> _matrix;
-	size_t              _rowIndex;
+	size_t _rowSize;
+	size_t _colSize;
 
-	// Will return start and end index of matrix;
-	pair<size_t, size_t> getValidIndexRange() {
-		pair<size_t, size_t> arrayBound{ -1,-1 };
+	vector<MatrixLocation> getPossiblePaths(MatrixLocation currentLocation) {
+		vector<MatrixLocation> possiblePath;
+		auto nextRow = currentLocation.first + 1;
 
-		arrayBound.first = (_rowIndex > 0) ? _rowIndex - 1 : _rowIndex;
-		arrayBound.second = (_rowIndex < _matrix[0].size() - 1) ? _rowIndex + 1 : _rowIndex;
+		possiblePath.emplace_back(MatrixLocation{ nextRow,currentLocation.second });
+		
+		// For item on the right
+		if (currentLocation.second >= 1)
+			possiblePath.emplace_back(MatrixLocation{ nextRow, currentLocation.second - 1 });
 
-		return arrayBound;
+		// For the item on the right
+		if (currentLocation.second < _colSize - 2)
+			possiblePath.emplace_back(MatrixLocation{ nextRow, currentLocation.second + 1 });
+
+		return possiblePath;
 	}
+
+	int getMinCost(const vector<MatrixLocation>& paths) {
+		vector<int> costs;
+		
+		// This is to handle empty inputs, all paths will have same first index
+		if (paths.size() == 0) return 0;
+		auto depth = paths[0].first;
+
+		if (depth != _rowSize - 1) {
+			for (const auto& point : paths) {
+				// Cacluting the lower path cost
+				auto newPath{ getPossiblePaths(point) };
+				auto cost = getMinCost(newPath);
+
+				// Adding the cost of the node also
+				cost += _matrix[point.first][point.second];
+				costs.emplace_back(cost);
+			}
+		}
+		else {
+			// This is the base case
+			for (const auto& point : paths)
+				costs.emplace_back(_matrix[point.first][point.second]);
+		}
+
+		auto minValue = min_element(costs.begin(), costs.end());
+		return *minValue;
+	};
 
 public:
 	int minFallingPathSum(vector<vector<int>>& A) {
-		int minWeight = 0;
-
-		// Some safe checking
-		if (A.size() == 0)
-			return minWeight;
-
-		// Intilizing the states
 		_matrix = A;
-		pair<size_t, size_t> subArrayPos{ 0, _matrix[0].size() - 1};
+		_rowSize = _matrix.size();
+		if (_rowSize == 0)
+			return 0;
+		_colSize = _matrix[0].size();
 
-		for (size_t row = 0; row < _matrix.size(); row++) {
-			auto matrixRow{ _matrix[row] };
-			auto minElement = min_element(matrixRow.begin() + subArrayPos.first, 
-								matrixRow.begin() + subArrayPos.second + 1);
-
-			// Adding the mininum weight
-			minWeight += *minElement;
-
-			// Getting the next index for the next row
-			_rowIndex = minElement - matrixRow.begin();
-			subArrayPos = getValidIndexRange();
+		// Intilizing the paths, the intial path can start from any point
+		vector<MatrixLocation> firstrowPaths;
+		for (size_t sz = 0; sz < A[0].size(); sz++) {
+			firstrowPaths.emplace_back(MatrixLocation{ 0,sz });
 		}
 
-		return minWeight;
+		return getMinCost(firstrowPaths);
 	}
 };
 
