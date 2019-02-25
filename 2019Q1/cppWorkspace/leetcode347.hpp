d18558552b57efbcea83388f454a531374a12f3a
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-02-25 15:34:45 +0530

    #14 Still have some bugs to solve
    Not giving correct results, for the second test case the number of nodes does not seems to be correct

--- 2019Q1/cppWorkspace/leetcode347.hpp
+++ 2019Q1/cppWorkspace/leetcode347.hpp
@@ -15,24 +15,63 @@ struct HeapNode {
 	HeapNode* left;
 	HeapNode* right;
 
-	HeapNode(int val) : itemCount(0), left(nullptr), right(nullptr) {}
+	HeapNode(int val) : item(val), itemCount(0), left(nullptr), right(nullptr) {}
 };
 
 class Solution {
 private:
-	HeapNode* _root;
+	vector<HeapNode*> heapNodes;
 
 	vector<int> getVectorFromHeap() {
-		return vector<int>{};
+		vector<int> items;
+		for (const auto& nd : heapNodes)
+			items.emplace_back(nd->item);
+
+		return items;
+	}
+
+	void addElementtoHeap(HeapNode* node, int val) {
+		if (node == nullptr)
+			return;
+
+		// Uninitilized node
+		if (node->item == 0 && node->itemCount == 0) {
+			node->item = val;
+			node->itemCount = 1;
+			return;
+		}
+
+		// If the same value increment count
+		if (node->item == val) {
+			node->itemCount += 1;
+			return;
+		}
+
+		if (val < node->item)
+			addElementtoHeap(node->left, val);
+		else
+			addElementtoHeap(node->right, val);
 	}
 
 public:
 	vector<int> topKFrequent(vector<int>& nums, int k) {
-		vector<HeapNode*> heapNodes;
 		
 		for (auto i = 0; i < k; i++)
-			heapNodes.emplace_back(new HeapNode(nums[i]));
+			heapNodes.emplace_back(new HeapNode(0));
 
+		for (int i = 0; i < (int)(k / 2); i++) {
+			auto leftIndex = 2 * i + 1;
+			auto rightIdnex = 2 * i + 2;
+
+			if (leftIndex < k)	heapNodes[i]->left = heapNodes[leftIndex];
+			if (rightIdnex < k) heapNodes[i]->right = heapNodes[rightIdnex];
+		}
+
+		// Always adding from the root
+		for (const auto& item : nums)
+			addElementtoHeap(heapNodes[0], item);
+
+		// Returning the values in the node, not the nodes itself
 		return getVectorFromHeap();
 	}
 };
@@ -48,10 +87,15 @@ void printvector(const T& arr) {
 int main() {
 	Solution s;
 	
-	// Test case 1
+	// Test case 1, should output 1,2
 	vector<int> input1{ 1,1,1,2,2,3 };
 	auto ret = s.topKFrequent(input1, 2);
 	printvector(ret);
+
+	// test case 2 should output 1,2,3
+	vector<int> input2{ 1,1,1,2,2,3,3,3,4 };
+	ret = s.topKFrequent(input2, 3);
+	printvector(ret);	
 
 	return 0;
 }
