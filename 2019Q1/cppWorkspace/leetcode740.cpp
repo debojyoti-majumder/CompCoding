37db2a982cc0576e272d292cfeb074d046d3f723
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-05-09 10:27:20 +0530

    #22 Still the TLE problem did not got solved
    Have look for some iterative solution

--- 2019Q1/cppWorkspace/leetcode740.cpp
+++ 2019Q1/cppWorkspace/leetcode740.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 class Solution {
 private:
+    map<string, int> _cache;
+
     map<int, size_t> buildMap(const vector<int>& vec) {
         map<int,size_t> returnedMap;
 
@@ -24,8 +26,19 @@ private:
             else
                 returnedMap.insert(make_pair(item, 1));
         }
-        
+
         return returnedMap;
+    }
+
+    template<typename KEY, typename VALUE>
+    string toString( const map<KEY,VALUE>& inputMap ) {
+        stringstream accmulator;
+
+        for( const auto& item : inputMap ) {
+            accmulator << item.first << "=" << item.second << " ";
+        }
+
+        return accmulator.str();
     }
 
     map<int, size_t> getReducedList(map<int, size_t> numberMap, map<int, size_t>::iterator candidateItem) {
@@ -49,6 +62,13 @@ private:
         // base case
         if( candidateItem == numberMap.end() || numberMap.size() == 0 ) return 0;
 
+        // Doing DP table lookup
+        string cacheKey { toString(numberMap) };
+        auto it { _cache.find(cacheKey) };
+        if( it != _cache.end() ) {
+            return it->second;
+        }
+
         // Case: Current index is part of the final solution
         auto reducedList { getReducedList(numberMap, candidateItem) };
         int selectedCost {  
@@ -60,7 +80,11 @@ private:
         auto startIndex { next(candidateItem) };
         int notSelectedCost { getMaxPoint(numberMap, startIndex) };
 
-        return max(selectedCost, notSelectedCost);
+        // Adding the value to the DP table
+        auto retVal { max(selectedCost, notSelectedCost) };
+        _cache.insert(make_pair(cacheKey, retVal));
+        
+        return retVal;
     }
 
 public:
@@ -76,16 +100,22 @@ public:
 void testLeetcode740() {
     Solution s;
     
-    vector<int> inp1{3,4,2};
-    vector<int> inp2{2,2,3,3,3,4};
-    vector<int> tleCase {1,8,5,9,6,9,4,1,7,3,3,6,3,3,8,2,6,3,2,2,1,2,9,8,7,1,1,10,6,7,3,9,6,10,5,4,10,1,6,7,4,7,4,1,9,5,1,5,7,5};
-        
     // Should output 6
+    vector<int> inp1{3,4,2};
     cout << s.deleteAndEarn(inp1) << endl;
     
     // Should output 9
+    vector<int> inp2{2,2,3,3,3,4};
     cout << s.deleteAndEarn(inp2) << endl;
 
-    // Should not get TLE, correct output 138
+    // // Should not get TLE, correct output 138
+    vector<int> tleCase {   1,8,5,9,6,9,4,1,7,3,3,6,3,3,8,2,6,3,2,2,1,2,9,8,
+                            7,1,1,10,6,7,3,9,6,10,5,4,10,1,6,7,4,7,4,1,9,5,1,5,7,5};
     cout << s.deleteAndEarn(tleCase) << endl;
+
+    vector<int> tleCase2 {  12,32,93,17,100,72,40,71,37,92,58,34,29,78,11,84,77,90,
+                            92,35,12,5,27,92,91,23,65,91,85,14,42,28,80,85,38,71,62,82,66,3,33,33,55,60,48,78,63,
+                            11,20,51,78,42,37,21,100,13,60,57,91,53,49,15,45,19,51,2,96,22,32,2,46,62,58,11,29,6,74,
+                            38,70,97,4,22,76,19,1,90,63,55,64,44,90,51,36,16,65,95,64,59,53,93};
+    cout << s.deleteAndEarn(tleCase2) << endl;
 }
