4bebd7ad7b94d8b4ef22cde42dabcc8dea8c8c6f
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-05-20 10:59:56 +0530

    #26 Solution accepted, RT can be improved
    Iterated the from reverse side, helped to accept the solution

--- 2019Q1/cppWorkspace/leetcode739.cpp
+++ 2019Q1/cppWorkspace/leetcode739.cpp
@@ -2,45 +2,56 @@
     Problem URL:    https://leetcode.com/problems/daily-temperatures/description/
     Problem ID:     739
     Issue Id:       26
+    
+    Related Problem:    
+        https://leetcode.com/problems/next-greater-element-i/
 */
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 class Solution {
+private:
+    int findMinDiff(const vector<pair<int,size_t>>& list, const pair<int,size_t>& candidate) {
+        if( list.empty() ) return 0;
+        size_t minVal = -1;
+        bool minFound = false;
+
+        for( const auto& item : list ) {
+            auto indexDiff { item.second - candidate.second };
+            if( item.first > candidate.first && indexDiff < minVal ) {
+                minVal = indexDiff;
+                minFound = true;
+            }
+        }
+
+        return minFound ? minVal : 0;
+    }
 
 public:
     vector<int> dailyTemperatures(vector<int>& T) {
         auto inputLength { T.size() };
+
+        // The wait list intialized, the last element is would always be zero
         vector<int> waitDaysList(inputLength, 0);
-        vector<size_t> waitList;
-        waitList.emplace_back(0);
+        waitDaysList[inputLength - 1] = 0;
+        vector<pair<int, size_t>> maxValueList;
+        maxValueList.emplace_back(make_pair(T[inputLength - 1], inputLength -1));
 
-        // Should do just one pass, the array is going to be long
-        for( size_t i=1; i<inputLength; i++ ) {
-            auto currentTemp { T[i] };
+        for( int i=inputLength - 2; i>=0; i-- ) {
+            auto currentTemp { make_pair(T[i],i) };
             
-            // Removing item from the wait list is we have got a hotter temp
-            auto remIt = remove_if(waitList.begin(), waitList.end(), [&](size_t index) {
-                auto temp { T[index] };
-                auto ret { currentTemp > temp };
-                
-                // If the item getting removed then we store the index
-                if( ret ) {
-                    auto diff { i - index };
-                    waitDaysList[index] = diff;
-                }
-
-                // returning the comparison result
-                return ret;
+            auto it = remove_if(maxValueList.begin(), maxValueList.end(),[&]( pair<int,size_t>& item ){
+                return item.first <= currentTemp.first;
             });
-            waitList.erase(remIt, waitList.end());
+            if( it != maxValueList.end() ) maxValueList.erase(it);
 
-            // By deault all the items would be added to the wait list
-            waitList.emplace_back(i);
+            waitDaysList[i] = findMinDiff(maxValueList, currentTemp);
+            maxValueList.emplace_back(currentTemp);
         }
 
         return waitDaysList;
@@ -59,11 +70,21 @@ void printVector(const vector<T>& list) {
 
 void testLeetcode739() {
     Solution s;
-    vector<int> inp{73, 74, 75, 71, 69, 72, 76, 73};
-
+    
     // Should output [1, 1, 4, 2, 1, 1, 0, 0]
+    vector<int> inp{73, 74, 75, 71, 69, 72, 76, 73};
     auto ret { s.dailyTemperatures(inp) };
     printVector(ret);
+    
+    // Should output [0,0,0,0,0]
+    vector<int> sameInp {53,53,53,53,53};
+    auto ret2 { s.dailyTemperatures(sameInp) };
+    printVector(ret2);
+
+    // Should output all zero
+    vector<int> huge(40000, 45);
+    auto ret3 { s.dailyTemperatures(huge) };
+    printVector(ret3);
 
 }
 
