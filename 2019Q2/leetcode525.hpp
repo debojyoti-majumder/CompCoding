06f9032d81defbcd5c6ce6d86fbde1087834701f
debojyotim <debojyotim@vmware.com>
2019-06-04 16:36:15 +0530

    Added some basic code for the problem

--- 2019Q2/leetcode525.hpp
+++ 2019Q2/leetcode525.hpp
@@ -8,9 +8,43 @@ namespace Leetcode525 {
     using namespace std;
 
     class Solution {
+        private:
+            vector<int> numbers;
+
+            int findMax(size_t pos, int contOneCount, int contZeroCount) {
+                auto sz { numbers.size() };
+                auto itemsRemaining { sz - pos };
+                auto retVal { 0 };
+
+                // Base cases
+                if( pos >= sz - 1 ) return 0;
+                if( contOneCount > itemsRemaining || contZeroCount > itemsRemaining ) return 0;
+
+                auto num { (bool)numbers[pos] };
+                auto nextNumber { (bool)numbers[pos +1] };
+                
+                // This means they are blanced
+                if( num == !(nextNumber) ) {
+                    return 2 + findMax(pos+2,0,0);
+                }
+
+                auto withoutOp { findMax(pos+1,0,0) };
+                if( num ) {
+                    auto withOneOp { findMax(pos+1,contOneCount+1,contZeroCount) };
+                    retVal = max(withOneOp,withOneOp);
+                }
+                else {
+                    auto withZeroOp { findMax(pos+1,contOneCount,contZeroCount+1) };
+                    retVal = max(withoutOp, withZeroOp);
+                }
+
+                return retVal;
+            }
+
         public:
             int findMaxLength(vector<int>& nums) {
-                return 0;
+                numbers = nums;
+                return findMax(0,0,0);
             }
     };
 
@@ -26,6 +60,13 @@ namespace Leetcode525 {
         vector<int> inp{0,1,0};
 
         EXPECT_EQ(s.findMaxLength(inp), 2);
+    }
+
+    GTEST_TEST(Prob525, LeadingZero) {
+        Solution s;
+        vector<int> inp { 0,0,0,0,1,1};
+
+        EXPECT_EQ(s.findMaxLength(inp), 4);
     }
 
     GTEST_TEST(Prob525, Ex3) {
