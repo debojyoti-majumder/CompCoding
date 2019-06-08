9ce412c15274f31c76106f60c5ac031ecc6dc56c
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-06-08 21:12:16 +0530

    #33 Brute force did not worked out well

--- 2019Q2/leetcode767.hpp
+++ 2019Q2/leetcode767.hpp
@@ -11,9 +11,42 @@ namespace Leetcode767 {
     using namespace std;
 
     class Solution {
+        private:
+            bool isTwoAdjacent(const string& str) {
+                auto sz{ str.size() };
+
+                for( size_t i=0; i<=sz-2; i++ ) {
+                    if( str[i] == str[i+1] )
+                        return true;
+                }
+
+                return false;
+            } 
+
+            string permuteString(string s, size_t swapIndex = 0) {
+                auto sz { s.size() };
+                
+                if( sz == swapIndex + 1 ) {
+                    if( false == isTwoAdjacent(s) )
+                        return s;
+                }
+                
+                for( size_t i=swapIndex; i<sz; i++ ) {
+                    auto nextItem{s};
+                    auto buff { nextItem[i] };
+                    nextItem[i] = nextItem[swapIndex];
+                    nextItem[swapIndex] = buff;
+
+                    auto ret = permuteString(nextItem, swapIndex+1);
+                    if( ret.size() != 0 ) return ret;
+                }
+
+                return "";
+            }
+
         public:
             string reorganizeString(string S) {
-                return "";
+                return permuteString(S);
             }
     };
 
@@ -25,5 +58,10 @@ namespace Leetcode767 {
     GTEST_TEST(Leet767, Test2) {
         Solution s;
         ASSERT_THAT(s.reorganizeString("aaab"), "");
+    }
+
+    GTEST_TEST(Leet767, TLECase1) {
+        Solution s;
+        ASSERT_THAT(s.reorganizeString("kkkkzrkatkwpkkkktrq"), "");
     }
 }
