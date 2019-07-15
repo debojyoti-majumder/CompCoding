654bb6e2fae519f78fcd1b8ff098d01b867d541e
debojyotim <debojyotim@vmware.com>
2019-07-15 14:41:51 +0530

    #36 Basic first impl seems okay

--- 2019Q2/leetcode388.hpp
+++ 2019Q2/leetcode388.hpp
@@ -7,16 +7,18 @@
 #include <sstream>
 #include <iostream>
 #include <stack>
+#include <algorithm>
 
 #include "gtest/gtest.h"
 
 namespace Leetcode388 {
     using namespace std;
-
+    
     class Solution {
     private:
         int _returnValue;
         int _currentLevel;
+        stack<int> _folderNameLengthStack;
 
         int getTabCount(const string& str) {
             auto tabCount = 0;
@@ -33,25 +35,53 @@ namespace Leetcode388 {
 
         void processToken(const string& str) {
             auto level { getTabCount(str) };    
+            auto folderName { str };
             auto dotIndex { str.find_first_of('.') };
 
             if( dotIndex == string::npos ) {
                 // This means it's folder
-
+                folderName.erase(remove(folderName.begin(), folderName.end(), '\t'), folderName.end());
+                auto folderNameLength { folderName.size() };
+                
+                if( level > _currentLevel ) {
+                    auto lastLen { _folderNameLengthStack.top() };
+                    _folderNameLengthStack.emplace(folderNameLength + lastLen);
+                }
+                else {
+                    if( false == _folderNameLengthStack.empty() ) {
+                        _folderNameLengthStack.pop();
+                        auto lastLen { _folderNameLengthStack.top() };
+                        _folderNameLengthStack.emplace(lastLen + folderNameLength);
+                    }
+                    else {
+                        _folderNameLengthStack.emplace(folderNameLength);
+                    }
+                }
             }
             else {
                 // This is for file
+                auto fileName { str };
+                fileName.erase(remove(fileName.begin(), fileName.end(), '\t'), fileName.end());
+                auto topItem { _folderNameLengthStack.top() };
+                auto pathLength { topItem + (int)fileName.length() };
+
+                if( pathLength > _returnValue )
+                    _returnValue = pathLength;
             }
+
+            _currentLevel = level;
         }
 
     public:
         int lengthLongestPath(string input) {
             stringstream inputStream(input);
             string token;
-
+            
             // Initilizing the states
             _returnValue = 0;
             _currentLevel = 0;
+            while(!_folderNameLengthStack.empty()) 
+                _folderNameLengthStack.pop();
 
             while(getline(inputStream, token)) {
                 processToken(token);
