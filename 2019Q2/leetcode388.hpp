1895e2d8d6edd25645da122800935694d944bd79
debojyotim <debojyotim@vmware.com>
2019-07-15 17:09:59 +0530

    #36 Some crash issues to be fixed

--- 2019Q2/leetcode388.hpp
+++ 2019Q2/leetcode388.hpp
@@ -45,13 +45,13 @@ namespace Leetcode388 {
                 
                 if( level > _currentLevel ) {
                     auto lastLen { _folderNameLengthStack.top() };
-                    _folderNameLengthStack.emplace(folderNameLength + lastLen);
+                    _folderNameLengthStack.emplace(folderNameLength + lastLen + 1);
                 }
                 else {
                     if( false == _folderNameLengthStack.empty() ) {
                         _folderNameLengthStack.pop();
                         auto lastLen { _folderNameLengthStack.top() };
-                        _folderNameLengthStack.emplace(lastLen + folderNameLength);
+                        _folderNameLengthStack.emplace(lastLen + folderNameLength + 1);
                     }
                     else {
                         _folderNameLengthStack.emplace(folderNameLength);
@@ -62,8 +62,13 @@ namespace Leetcode388 {
                 // This is for file
                 auto fileName { str };
                 fileName.erase(remove(fileName.begin(), fileName.end(), '\t'), fileName.end());
-                auto topItem { _folderNameLengthStack.top() };
-                auto pathLength { topItem + (int)fileName.length() };
+                auto pathLength { (int)fileName.length() };
+
+                // If the stack is empty then it is just a file name
+                if( false == _folderNameLengthStack.empty() ) {
+                    auto topItem { _folderNameLengthStack.top() };
+                    pathLength += 1 + topItem;
+                }
 
                 if( pathLength > _returnValue )
                     _returnValue = pathLength;
@@ -93,8 +98,15 @@ namespace Leetcode388 {
 
     GTEST_TEST(Leetcode388, BasicTest) {
         auto inp{"dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"};
+        auto inp2{"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"};
+        auto inp3 {"a.txt"};
+        auto inp4 {"a\n\tb.txt\na2\n\tb2.txt"};
+
         Solution s;
 
         EXPECT_EQ(s.lengthLongestPath(inp), 20);
+        EXPECT_EQ(s.lengthLongestPath(inp2), 32);
+        EXPECT_EQ(s.lengthLongestPath(inp3), 5);
+        EXPECT_EQ(s.lengthLongestPath(inp4), 9);
     }
 }
