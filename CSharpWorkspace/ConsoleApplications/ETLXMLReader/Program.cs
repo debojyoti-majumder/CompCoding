3d3b62ca442bf511b2a05c4c4aa9454dbaf47aed
debojyotim <debojyotim@vmware.com>
2019-07-31 13:05:52 +0530

    Basic parsing happening correctly

---
+++ CSharpWorkspace/ConsoleApplications/ETLXMLReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace ETLXMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if( args.Length == 0 )
+            {
+                Console.WriteLine("Provide the etl XML file");
+                return;
+            }
+
+            var fileName = args[0];
+            if( !File.Exists(fileName) )
+            {
+                Console.WriteLine("{0} file is does not exsists", fileName);
+                return;
+            }
+
+            var xmlTextReader = new XmlDocument();
+            xmlTextReader.Load(fileName);
+            int count = 0;
+
+            foreach( var childNode in xmlTextReader.DocumentElement.ChildNodes)
+            {
+                count++;
+            }
+
+            Console.WriteLine("{0} event processes", count);
+        }
+    }
+}
