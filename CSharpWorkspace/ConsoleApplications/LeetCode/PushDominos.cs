96520eb1fe998e0e2bf403cbf3173517f4e32fa8
debojyotim <debojyotim@vmware.com>
2019-08-01 15:10:26 +0530

    Not terminating and wrong ouput

--- CSharpWorkspace/ConsoleApplications/LeetCode/PushDominos.cs
+++ CSharpWorkspace/ConsoleApplications/LeetCode/PushDominos.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 
 // Problem URL: https://leetcode.com/problems/push-dominoes/
 
@@ -9,9 +6,53 @@ namespace PushDominos
 {
     public class Solution
     {
+        private StringBuilder _outputString;
+        
+        private bool IsToBeAltered(bool isLeft, int index)
+        {
+            if( isLeft )
+            {
+                if (index == 0) return false;
+                else if (index == 1) return true;
+                else return _outputString[index - 2] != 'R';
+
+            }
+            else {
+                if (index == _outputString.Length - 1) return false;
+                else if (index == _outputString.Length - 2) return true;
+                else return _outputString[index + 2] != 'L';
+            }
+        }
+
         public string PushDominoes(string dominoes)
         {
-            return dominoes;
+            bool isChangesMade = false;
+            _outputString = new StringBuilder(dominoes);
+
+            for(int i=0; i<_outputString.Length; i++)
+            {
+                if( dominoes[i] == 'L' && IsToBeAltered(true, i) )
+                {
+                    if (isChangesMade == false)
+                        isChangesMade = true;
+
+                    _outputString[i - 1] = 'L';
+                    
+                }
+                else if ( dominoes[i] == 'R' && IsToBeAltered(false, i))
+                {
+
+                    if (isChangesMade == false)
+                        isChangesMade = true;
+
+                    _outputString[i+1] = 'R';
+                }
+            }
+
+            if (isChangesMade == false)
+                return _outputString.ToString();
+
+            return PushDominoes(dominoes);
         }
     }
 }
