2007480a8985e0eeec322499b5ed294fc281d926
debojyotim <debojyotim@vmware.com>
2019-08-01 11:53:37 +0530

    Added leetcode project

---
+++ CSharpWorkspace/ConsoleApplications/LeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PushDominos.Solution s = new PushDominos.Solution();
+
+            // Should ouput "LL.RR.LLRRLL.."
+            Console.WriteLine(s.PushDominoes(".L.R...LR..L.."));
+
+            // Should output "RR.L"
+            Console.WriteLine(s.PushDominoes("RR.L"));
+        }
+    }
+}
---
+++ CSharpWorkspace/ConsoleApplications/LeetCode/PushDominos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Problem URL: https://leetcode.com/problems/push-dominoes/
+
+namespace PushDominos
+{
+    public class Solution
+    {
+        public string PushDominoes(string dominoes)
+        {
+            return dominoes;
+        }
+    }
+}
