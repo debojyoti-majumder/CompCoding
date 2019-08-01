a224c6bce9801f0ddb5cdb4410db8d223826a184
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2019-08-01 20:00:35 +0530

    Basic Traversal done, building needs correcting

--- CSharpWorkspace/ConsoleApplications/CollectionApp1/Program.cs
+++ CSharpWorkspace/ConsoleApplications/CollectionApp1/Program.cs
@@ -6,7 +6,16 @@ namespace CollectionApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Smaple comment");
+            Console.WriteLine("Building a tree with 10 nodes");
+
+            var rootNode = TreeDepthTraverser.TreeNodeBuilder.BuildTreeNodes(5);
+            TreeDepthTraverser.TreeTravrser trvs = new TreeDepthTraverser.TreeTravrser(rootNode);
+            Console.WriteLine("Depth:{0}", trvs.Depth);
+            int lev = int.Parse(Console.ReadLine());
+            var itemsAtLevel = trvs.GetNodesAtDepth(lev);
+            foreach (var d in itemsAtLevel)
+                Console.Write("{0} ", d);
+            Console.WriteLine();
         }
     }
 }
---
+++ CSharpWorkspace/ConsoleApplications/CollectionApp1/TreeDepthTraverser.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeDepthTraverser
+{
+    class TreeNode
+    {
+        public int Data;
+        public TreeNode LeftNode;
+        public TreeNode RightNode;
+    }
+
+    class TreeNodeBuilder
+    {
+        public static TreeNode BuildTreeNodes(int numberOfNodes, int currentCount = 0)
+        {
+            TreeNode nd = new TreeNode
+            {
+                Data = currentCount,
+                LeftNode = null,
+                RightNode = null
+            };
+
+            if (currentCount < numberOfNodes)
+            {
+                nd.LeftNode = BuildTreeNodes(numberOfNodes, currentCount + 1);
+                nd.RightNode = BuildTreeNodes(numberOfNodes, currentCount + 2);
+            }
+
+            return nd;
+        }
+    }
+
+    class TreeTravrser
+    {
+        private TreeNode _root;
+        private int _depth;
+
+        public TreeTravrser(TreeNode n)
+        {
+            _root = n;
+            _depth = -1;
+        }
+
+        private int CalculateDepth(TreeNode nd)
+        {
+            if (nd == null)
+                return 0;
+
+            int leftDepth = CalculateDepth(nd.LeftNode);
+            int rightDepth = CalculateDepth(nd.RightNode);
+
+            return 1 + Math.Max(leftDepth, rightDepth);
+        }
+
+        public int Depth
+        {
+            get
+            {
+                if (_depth == -1)
+                    _depth = CalculateDepth(_root);
+
+                return _depth;
+            }
+        }
+
+        private List<int> GetResultsOnDepth(TreeNode nd, int d, int currentDepth = 1)
+        {
+            if (currentDepth == d)
+            {
+                return new List<int> { nd.Data };
+            };
+
+            var leftSide = GetResultsOnDepth(nd.LeftNode, d, currentDepth + 1);
+            var rightSide = GetResultsOnDepth(nd.RightNode, d, currentDepth + 1);
+            leftSide.AddRange(rightSide);
+
+            // This is the resultant item
+            return leftSide;
+        }
+
+        public List<int> GetNodesAtDepth(int depth)
+        {
+            if (depth > this.Depth)
+                throw new Exception("Bad argument");
+
+            return GetResultsOnDepth(_root, depth);
+        }
+    }
+}
