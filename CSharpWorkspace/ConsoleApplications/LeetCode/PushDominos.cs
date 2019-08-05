0eaef485dc073cee86b9d5d2ba32b7e3e39e6da0
debojyotim <debojyotim@vmware.com>
2019-08-05 09:04:30 +0530

    Second recursive call needs to fixed

--- CSharpWorkspace/ConsoleApplications/LeetCode/PushDominos.cs
+++ CSharpWorkspace/ConsoleApplications/LeetCode/PushDominos.cs
@@ -7,54 +7,50 @@ namespace PushDominos
     public class Solution
     {
         private StringBuilder _outputString;
-        
+
         private bool IsToBeAltered(bool isLeft, int index)
         {
-            if( isLeft )
+            if (isLeft)
             {
                 if (index == 0) return false;
-                else return _outputString[index - 1] == '.';
+                else return _outputString[index - 1] != 'L';
             }
-            else {
+            else
+            {
                 if (index == _outputString.Length - 1) return false;
-                else return _outputString[index + 1] == '.';
+                else return _outputString[index + 1] != 'R';
             }
         }
 
         private bool CanStandStill(int index)
         {
+            if (_outputString[index] != '.') return false;
+
             if (index == 0 || index == _outputString.Length) return false;
-            else return false; 
+            else return _outputString[index - 1] == 'R' && _outputString[index + 1] == 'L';
         }
 
         public string PushDominoes(string dominoes)
         {
-            bool isChangesMade = false;
             _outputString = new StringBuilder(dominoes);
 
-            for(int i=0; i<_outputString.Length; i++)
+            for (int i = 0; i < _outputString.Length; i++)
             {
+                if (CanStandStill(i))
+                    continue;
 
-                if( dominoes[i] == 'L' && IsToBeAltered(true, i) )
+                if (dominoes[i] == 'L' && IsToBeAltered(true, i))
                 {
-                    if (isChangesMade == false)
-                        isChangesMade = true;
-
                     _outputString[i - 1] = 'L';
-                    
                 }
-                else if ( dominoes[i] == 'R' && IsToBeAltered(false, i))
+                else if (dominoes[i] == 'R' && IsToBeAltered(false, i))
                 {
-
-                    if (isChangesMade == false)
-                        isChangesMade = true;
-
-                    _outputString[i+1] = 'R';
+                    _outputString[i + 1] = 'R';
                 }
             }
 
             var processedString = _outputString.ToString();
-            if (isChangesMade == false)
+            if (processedString == dominoes)
                 return processedString;
 
             return PushDominoes(processedString);
