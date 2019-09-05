cac34dfda3b8a790dd07f9edb4fd8f58f12a19fc
debojyotim <debojyotim@vmware.com>
2019-09-05 15:39:51 +0530

    Added basic implementation

--- Worksp20/JavaWorkspace/leetcode/src/main/java/com/leetcode/FruitBasketCollector.java
+++ Worksp20/JavaWorkspace/leetcode/src/main/java/com/leetcode/FruitBasketCollector.java
@@ -4,12 +4,82 @@
 package com.leetcode;
 
 public class FruitBasketCollector {
-    public FruitBasketCollector(int[] trees) {
-        // Just some dummy code
-        trees[0] = 0;
+    class FruitBucket {
+        public int fruitType;
+        public int count;
+
+        public FruitBucket() {
+            // Indicates empty
+            fruitType = -1;
+            count = 0;
+        }
+
+        public boolean isEmpty() {
+            return fruitType == -1;
+        }
     }
 
-    public int getMaxScore() {
-        return 0;
+    private FruitBucket _bucketOne;
+    private FruitBucket _bucketTwo;
+    private int[] _treeArray;
+
+    public FruitBasketCollector(int[] trees) {
+        _bucketOne = new FruitBucket();
+        _bucketTwo = new FruitBucket();
+        _treeArray = trees;
+    }
+
+    private boolean updateBucket(int type) {
+        if( _bucketOne.isEmpty() ) {
+            _bucketOne.count = 1;
+            _bucketOne.fruitType = type;
+            return true;
+        }
+
+        if( _bucketTwo.isEmpty() ) {
+            _bucketTwo.count = 1;
+            _bucketTwo.fruitType = type;
+            return true;
+        }
+
+        if( _bucketOne.fruitType == type ) {
+            _bucketOne.count++;
+            return true;
+        }
+
+        if( _bucketTwo.fruitType == type ) {
+            _bucketTwo.count++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getMaxScore(int startIndex) {
+        int retVal = 0;
+        int numberOfTrees = _treeArray.length;
+        int i = 0;
+
+        // This should be in order of n
+        for( i=startIndex; i<numberOfTrees; i++ ) {
+            if ( !updateBucket(_treeArray[i]) )
+                break;
+        }
+
+        // Add to retrun value if it's not empty
+        if( false == _bucketOne.isEmpty() ) retVal += _bucketOne.count;
+        if( false == _bucketTwo.isEmpty() ) retVal += _bucketTwo.count;
+        
+        // That means we had to stop
+        if( i != numberOfTrees ) {
+            _bucketOne = new FruitBucket();
+            _bucketTwo = new FruitBucket();
+
+            // Get the max value
+            int startOverValue = getMaxScore(i-1);
+            retVal = Math.max(retVal, startOverValue);
+        }
+
+        return retVal;
     }
 }
--- Worksp20/JavaWorkspace/leetcode/src/test/java/com/leetcode/FruitBasketCollectorTest.java
+++ Worksp20/JavaWorkspace/leetcode/src/test/java/com/leetcode/FruitBasketCollectorTest.java
@@ -7,14 +7,20 @@ public class FruitBasketCollectorTest {
     @Test
     public void basicTests() {
         FruitBasketCollector cl1 = new FruitBasketCollector(new int[] {1,2,1});
-        assertEquals(3, cl1.getMaxScore());
+        assertEquals(3, cl1.getMaxScore(0));
 
         // Explanation: We can collect [1,2,2], If we started at the first tree, we would only collect [0, 1].
         FruitBasketCollector cl2 = new FruitBasketCollector(new int[]{0,1,2,2});
-        assertEquals(3, cl2.getMaxScore());
+        assertEquals(3, cl2.getMaxScore(0));
 
         // If we started at the first tree, we would only collect [1, 2].
         FruitBasketCollector cl3 = new FruitBasketCollector(new int[]{1,2,3,2,2});
-        assertEquals(4, cl3.getMaxScore());
+        assertEquals(4, cl3.getMaxScore(0));
+    }
+
+    @Test
+    public void backTrackTest() {
+        FruitBasketCollector cl4 = new FruitBasketCollector(new int[]{1,1,2,2,3,3,3,3});
+        assertEquals(6, cl4.getMaxScore(0));
     }
 }
