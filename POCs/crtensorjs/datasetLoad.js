757ae0b0019006001647656b6a9a9f0eb619bbb9
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-03-08 21:59:01 +0530

    Now this seems to be working fine

--- POCs/crtensorjs/datasetLoad.js
+++ POCs/crtensorjs/datasetLoad.js
@@ -1,19 +1,23 @@
 // Works on iris data set, Usage : node loadData.js -c -d "iris.csv"
 
 const tf = require('@tensorflow/tfjs');
+const commandLineArgs = require('command-line-args');
+
+// This is to host the data file
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('data'));
 
 // for command line parsing
-const commandLineArgs = require('command-line-args')
 const optionDefinitions = [
     { name: 'cpu', alias: 'c', type: Boolean, },
-    // { name: 'src', type: String, multiple: true, defaultOption: true },
     { name: 'data', alias: 'd', type: String },
-    { name: 'timeout', alias: 't', type: Number }];
+    { name: 'timeout', alias: 't', type: Number }
+];
 
 const parsedOptions = commandLineArgs(optionDefinitions);
-process.stdin.setRawMode(true);
-process.stdin.resume();
-process.stdin.on('data', process.exit.bind(process, 0));
+
 
 // Optional dependency to have cpu accelaration
 if( parsedOptions['cpu'] !== null && parsedOptions['cpu'] === true ) {
@@ -23,64 +27,47 @@ if( parsedOptions['cpu'] !== null && parsedOptions['cpu'] === true ) {
 
 // The data option is needed of course to load the dataset.
 if( parsedOptions['data'] !== null ) {
-    const dataFile = parsedOptions['data'];
-    run(dataFile);
+    app.listen(port, () => {
+        console.log("Web server is running on port", port);
+
+        const dataFile = parsedOptions['data'];
+        const trainedModel = run(dataFile);
+        console.log(trainedModel);    
+    });    
 } 
 
 async function run(dataFile) {
     console.log("Running the model with data file", dataFile);
-
-    const trainingData = tf.data.csv(dataFile,{
+    const trainingData = tf.data.csv(dataFile, {
         columnConfigs: {
-            species: { isLable: true }     
+            species: {
+                isLabel: true
+            }
         }
     });
-
-    const batchSize = 10;
-    const numberOfFeatures = 4;
-
-    // This is been done as one hot encoding
-    const convertedData = trainingData.map(({xs,ys}) => {
-        const labels = [
-            ys.species == "setosa" ? 1 : 0,
-            ys.species == "virginica" ? 1 : 0,
-            ys.species == "versicolor" ? 1 : 0 
-        ]
-
-        return {xs:Object.values(xs), ys:Object.values(labels)};
-    }).batch(batchSize);
-
+    
+    const numOfFeatures = (await trainingData.columnNames()).length - 1;
+    const convertedData = trainingData.map(({xs, ys}) => {
+              const labels = [
+                    ys.species == "setosa" ? 1 : 0,
+                    ys.species == "virginica" ? 1 : 0,
+                    ys.species == "versicolor" ? 1 : 0
+              ] 
+              return{ xs: Object.values(xs), ys: Object.values(labels)};
+          }).batch(15);
+    
     const model = tf.sequential();
-    const inputLayer = tf.layers.dense({
-        inputShape: [numberOfFeatures],
-        activation: 'sigmoid',
-        units: 5
-    });
-
-    // 3 because the of number species
-    const outputLayer = tf.layers.dense({
-        activation: 'softmax',
-        units: 3
-    });
-
-    model.add(inputLayer);
-    model.add(outputLayer);
-
-    model.compile({
-        loss: "categoricalCrossentropy",
-        optimizer: tf.train.adam(0.05)
-    });
-
-    await model.fitDataset(
-        convertedData,
-        {
-            epochs: 200,
-            callbacks: {
-                onEpochEnd: async(epoch, logs) => {
-                    console.log("Epoch# " + epoch + "Loss: " + logs.loss);
-                }
-            }
-        }  
-    );
-    console.log(trainingData, convertedData);
+    model.add(tf.layers.dense({inputShape: [numOfFeatures], activation: "sigmoid", units: 5}))
+    model.add(tf.layers.dense({activation: "softmax", units: 3}));
+    
+    model.compile({loss: "categoricalCrossentropy", optimizer: tf.train.adam(0.06)});
+    
+    await model.fitDataset(convertedData, 
+                     {epochs:200,
+                      callbacks:{
+                          onEpochEnd: async(epoch, logs) =>{
+                              console.log("Epoch: " + epoch + " Loss: " + logs.loss);
+                          }
+                      }});
+    return model
 }
