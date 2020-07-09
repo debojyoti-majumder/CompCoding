ebd6d06c9119d20c18cd529f455f6f2e2d3f273d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-09 12:15:14 +0530

    Setting up for a good testable workspace

---
+++ CodeWars/SharpArea/Year2020/Part1/CustomMath.cs
@@ -0,0 +1,39 @@
+﻿// Problem URL: https://www.codewars.com/kata/50654ddff44f800200000004/train/csharp
+
+using NUnit.Framework;
+using System;
+
+namespace Part1
+{
+    public class CustomMath
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        [TestFixture]
+        public class TestCases
+        {
+            [Test]
+            public void SimpleTest()
+            {
+                Assert.AreEqual(6, Multiply(2, 3));
+                Assert.AreEqual(0, Multiply(0, 6));
+            }
+
+            [Test]
+            public void BasicPropertytest()
+            {
+                var generator = new Random();
+
+                for( int i=0; i<10; i++ )
+                {
+                    int data = generator.Next();
+                    Assert.AreEqual(0, Multiply(0, data));
+                    Assert.AreEqual(data, Multiply(1, data));
+                }
+            }
+        }
+    }
+}
---
+++ CodeWars/SharpArea/Year2020/Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);            
+        }
+    }
+}
