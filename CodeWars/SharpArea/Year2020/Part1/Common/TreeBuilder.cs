7cd9ad7be9424f0a89f06f5979265106c8cbf64e
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-09 14:15:19 +0530

    Adding Treenode related items

---
+++ CodeWars/SharpArea/Year2020/Part1/Common/TreeBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace Part1.Common
+{
+    public class TreeBuilder
+    {
+        public static TreeNode SimpleTree()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode nd1 = new TreeNode(3);
+            TreeNode nd2 = new TreeNode(2);
+            TreeNode nd3 = new TreeNode(5);
+            TreeNode nd4 = new TreeNode(3);
+            TreeNode nd5 = new TreeNode(9);
+
+            // Making the edges
+            root.left = nd1;
+            root.right = nd2;
+            nd1.left = nd3;
+            nd1.right = nd4;
+            nd2.right = nd5;
+
+            return root;
+        }
+    }
+}
---
+++ CodeWars/SharpArea/Year2020/Part1/Common/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
