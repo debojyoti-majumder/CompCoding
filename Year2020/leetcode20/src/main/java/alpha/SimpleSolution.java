e0332ae26553be5b7ec427a0140af079ac022ac8
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-22 12:41:33 +0530

    Now maven project setup working fine

---
+++ Year2020/leetcode20/src/main/java/alpha/SimpleSolution.java
@@ -0,0 +1,7 @@
+package alpha;
+
+public class SimpleSolution {
+    public int addNumbers(int x, int y) {
+        return x + y;
+    }
+}
---
+++ Year2020/leetcode20/src/test/java/alpha/SimpleTest.java
@@ -0,0 +1,9 @@
+package alpha;
+
+public class SimpleTest {
+
+    public void BasicTests() {
+        SimpleSolution s = new SimpleSolution();
+        s.addNumbers(2,3);
+    }
+}
