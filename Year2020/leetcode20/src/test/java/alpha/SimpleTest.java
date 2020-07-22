5b1ac01e9aa0dca39e84c01acba67dfe010aea09
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-22 12:55:11 +0530

    Added a sample JUnit test
    This is simple solution, but would use the same mechanism. Need to add Timeout operation example.

--- Year2020/leetcode20/src/test/java/alpha/SimpleTest.java
+++ Year2020/leetcode20/src/test/java/alpha/SimpleTest.java
@@ -1,9 +1,22 @@
 package alpha;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 public class SimpleTest {
 
+    @Test
     public void BasicTests() {
         SimpleSolution s = new SimpleSolution();
-        s.addNumbers(2,3);
+        var retVal = s.addNumbers(2,3);
+
+        Assertions.assertEquals(retVal, 5);
+
+        Random rnd = new Random();
+        var positiveNumber = rnd.nextInt(200);
+        var negNumber = -1 * positiveNumber;
+        Assertions.assertEquals(s.addNumbers(positiveNumber, negNumber), 0);
     }
 }
