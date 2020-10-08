af9b1d90c5da74c19b86000d17e9576b9aaefcd3
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-10-08 13:52:32 +0530

    Added a simple bench mark implementation without verification

--- Year2020/oct/randomConsttime.cpp
+++ Year2020/oct/randomConsttime.cpp
@@ -1,8 +1,11 @@
 // URL: https://leetcode.com/problems/insert-delete-getrandom-o1/
 
 #include "pch.h"
+
 #include <iostream>
 #include <random>
+#include <chrono>
+#include <vector>
 
 using namespace std;
 
@@ -35,15 +38,28 @@ public:
 };
 
 class RandomizedVector {
+private:
+    vector<int>     _numbers;
+    random_device   _rd;
+    mt19937         _generator;
+
 public:
     /** Initialize your data structure here. */
-    RandomizedVector() {
-
+    RandomizedVector() : _generator(_rd()) {
+        
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        return true;
+        // Over time this would take some time
+        auto it = find(_numbers.begin(), _numbers.end(), val);
+        
+        if (it == _numbers.end()) {
+            _numbers.push_back(val);
+            return true;
+        }
+        else
+            return false;
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
@@ -53,12 +69,17 @@ public:
 
     /** Get a random element from the set. */
     int getRandom() {
-        return 0;
+        uniform_int_distribution<int> dist(0, _numbers.size() - 1);
+        auto randomIndex = dist(_generator);
+
+        return _numbers[randomIndex];
     }
 };
 
-void testRandomSet() {
+vector<int> testRandomSet() {
     auto randomSource = new RandomizedSet();
+    chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    vector<int> returnValue;
 
     random_device rd;
     mt19937 generator(rd());
@@ -73,21 +94,27 @@ void testRandomSet() {
         }
 
         if (i % 10 == 0) {
-            cout << randomSource->getRandom();
+            auto n{ randomSource->getRandom() };
+            returnValue.emplace_back(n);
         }
     }
 
-    cout << endl;
+    chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    cout << "Time difference = " << chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mcs]" << endl;
+
+    return returnValue;
 }
 
-void testRandomVector() {
+vector<int> testRandomVector() {
     auto randomSource = new RandomizedVector();
-
+    chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    vector<int> returnValue;
+    
     random_device rd;
     mt19937 generator(rd());
     uniform_int_distribution<int> distribution(1, ARR_SIZE);
 
-    for (int i = 0; i < 3000; i++) {
+    for (int i = 0; i < 30000; i++) {
         int number = distribution(generator);
         randomSource->insert(number);
 
@@ -96,14 +123,20 @@ void testRandomVector() {
         }
 
         if (i % 10 == 0) {
-            cout << randomSource->getRandom() << " ";
+            auto n{ randomSource->getRandom() };
+            returnValue.emplace_back(n);
         }
     }
 
-    cout << endl;
+    chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    cout << "Time difference with naive impl = " << chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mcs]" << std::endl;
+
+    return returnValue;
 }
 
 int main() {
-    std::cout << "Hello World!\n"; 
+    testRandomSet();
+    testRandomVector();
+
     return 0;
 }
