fc91b81c0fdb2a4e6997190d21dcd2e7365be7f3
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2021-02-16 16:00:11 +0530

    Failed test case from the problem to be added later

--- golangLeetcode/febProbPart1_test.go
+++ golangLeetcode/febProbPart1_test.go
@@ -31,3 +31,30 @@ func TestDiagonalSum(t *testing.T) {
 		t.Errorf("Expected value to be 18")
 	}
 }
+
+// Problem URL: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+func TestIsRoatedAndSorted(t *testing.T) {
+	numbers1 := []int{1, 2, 3, 4, 5}
+	numbers2 := []int{3, 4, 5, 1, 2}
+	numbers3 := []int{2, 1, 3, 4}
+	numbers4 := []int{1, 1, 1}
+
+	// This seems to be the failing case
+	numbers5 := []int{9, 6, 6, 7}
+
+	if !checkIncRotaed(numbers1) {
+		t.Errorf("The array is fine")
+	}
+
+	if !checkIncRotaed(numbers2) {
+		t.Errorf("Both the arrays are fine")
+	}
+
+	if checkIncRotaed(numbers3) && checkIncRotaed(numbers4) {
+		t.Errorf("Negavitve test case are not handled correctly")
+	}
+
+	if !checkIncRotaed(numbers5) {
+		t.Errorf("Number rotation at the last does not seems to be working")
+	}
+}
