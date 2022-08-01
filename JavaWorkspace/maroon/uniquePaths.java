ab6a7937ae376fc431099fc96b983c23c391b9e3
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2022-08-01 22:47:05 +0530

    Had some bugs, fixed them
    Still needs to be debugged

--- JavaWorkspace/maroon/uniquePaths.java
+++ JavaWorkspace/maroon/uniquePaths.java
@@ -12,51 +12,38 @@ import java.util.Optional;
 
 public class UniquePath {
     private static class VisitBox {
-        public int x;
-        public int y;
+        private int x;
+        private int y;
         private List<VisitBox> visitBacktracking;
+        public int nodeID;
 
-        public VisitBox(int x, int y) {
-            this.x = x;
-            this.y = y;
-
+        public VisitBox(int x, int y, int n) {
+            this.nodeID = x*n + y + 1;
             this.visitBacktracking = new ArrayList<>();
         }
 
         public List<VisitBox> getAdjacentBoxes(int m, int n) {
             List<VisitBox> adjcItems = new ArrayList<>();
 
-            if( x > 0 ) adjcItems.add(new VisitBox(this.x - 1, this.y));
-            if( x < m -1 ) adjcItems.add(new VisitBox(this.x + 1, this.y));
-            if( y > 0 ) adjcItems.add(new VisitBox(this.x, this.y - 1));
-            if( y < n -1 ) adjcItems.add(new VisitBox(this.x, this.y + 1));
+            if( y > 0 ) adjcItems.add(new VisitBox(this.x, this.y - 1,n));
+            if( y < n - 1 ) adjcItems.add(new VisitBox(this.x, this.y + 1,n));
+            if( x < m - 1 ) adjcItems.add(new VisitBox(this.x + 1, this.y,n));
+            if( x > 0 ) adjcItems.add(new VisitBox(this.x - 1, this.y,n));
 
             return adjcItems;
         }
 
         public boolean visit(VisitBox b) {
-            var selfLoop = b.visitBacktracking.stream()
-                .filter(obj ->  obj.x == this.x && obj.y == this.y)
+            this.visitBacktracking = b.visitBacktracking;
+            Optional<VisitBox> selfItem = this.visitBacktracking.stream()
+                .filter(obj -> obj.nodeID == b.nodeID)
                 .findFirst();
             
-            if( selfLoop.isPresent() ) return false;
-
-            Optional<VisitBox> alreadyVisited = this.visitBacktracking.stream()
-                .filter(obj ->  obj.x == b.x && obj.y == b.y)
-                .findFirst();
-            
-            // This means this is new path tha
-            if( !alreadyVisited.isPresent() ) {
-                this.visitBacktracking.add(b);
-                return true;
-            }
-            else {
-                return false;
-            }
+            return selfItem.isEmpty();
         }
 
         public boolean isDestination(int m, int n) {
-            return this.x == m && this.y == n;
+            return this.x == m-1 && this.y == n-1;
         }
     }
 
@@ -66,7 +53,7 @@ public class UniquePath {
         VisitBox[][] cellMatrix = new VisitBox[m][n];
         for( int i=0; i<m; i++ ) {
             for( int j=0; j<n; j++) {
-                cellMatrix[i][j] = new VisitBox(i, j);
+                cellMatrix[i][j] = new VisitBox(i, j, n);
             }
         }
 
@@ -84,19 +71,25 @@ public class UniquePath {
         // This is the starting point 
         visitQueue.add(this.inputMatrix[0][0]);
         while(visitQueue.isEmpty() == false) {
-            VisitBox box = visitQueue.poll();
+            VisitBox currentItem = visitQueue.poll();
             
-            // Iterating through it's nodes
-            var linkedItems = box.getAdjacentBoxes(m, n);
+            // If destination is reach no need to look for further nodes
+            if( currentItem.isDestination(m, n) ) {
+                uniquePathCount += 1;
+                continue;
+            }
+
+            // Iterating through it's nodes, these are just simple 
+            // indexes does not mean anything
+            var linkedItems = currentItem.getAdjacentBoxes(m, n);
 
             // Any newly linked node should be visited
             for( var lnk : linkedItems ) {
-                boolean isNewNode = lnk.visit(box);
-                if( isNewNode ) visitQueue.add(lnk);
+                boolean isNewNode = lnk.visit(currentItem);
+                if( isNewNode ) {
+                    visitQueue.add(lnk);
+                }
             }
-
-            // We have found new path to go
-            if( box.isDestination(m, n) ) uniquePathCount += 1;
         }
         
         return uniquePathCount;
